bfb123d1f17be859e85af7ecbeb82142d697ffaf
Brian Rebel <brebel@fnal.gov>
2013-11-06 17:15:14 +0000

    fix typos

--- lbne/Utilities/test/SignalShapingLBNE10ktTest_module.cc
+++ lbne/Utilities/test/SignalShapingLBNE10ktTest_module.cc
@@ -78,7 +78,7 @@ namespace util
   DEFINE_ART_MODULE(SignalShapingLBNE10ktTest)
 
   SignalShapingLBNE10ktTest::SignalShapingLBNE10ktTest(const fhicl::ParameterSet& pset)
-  : EDAnalzyer(pset)
+  : EDAnalyzer(pset)
   {}
 
   void SignalShapingLBNE10ktTest::beginJob()
