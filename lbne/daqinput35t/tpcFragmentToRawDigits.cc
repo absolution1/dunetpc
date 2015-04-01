5fb6ac741594628666fe2a3f98a37ed621be1ec8
Kyle Knoepfel <knoepfel@fnal.gov>
2015-04-01 13:23:33 -0500

    First working version of SplitterInput

--- lbne/daqinput35t/tpcFragmentToRawDigits.cc
+++ lbne/daqinput35t/tpcFragmentToRawDigits.cc
@@ -17,17 +17,17 @@
 
 std::vector<raw::RawDigit>
 DAQToOffline::tpcFragmentToRawDigits(art::EventID const& eid, artdaq::Fragments const& rawFragments, bool debug,
-				     raw::Compress_t compression, unsigned int zeroThreshold)
-{  
+                                     raw::Compress_t compression, unsigned int zeroThreshold)
+{
   //Create a map containing (fragmentID, fragIndex) for the event, will be used to check if each channel is present
   unsigned int numFragments = rawFragments.size();
 
   std::map < unsigned int, unsigned int > mapFragID;
-  
+
   for(size_t fragIndex = 0; fragIndex < rawFragments.size(); fragIndex++){
-    
+
     const artdaq::Fragment &singleFragment = rawFragments[fragIndex];
-    
+
     unsigned int fragmentID = singleFragment.fragmentID();
 
     mapFragID.insert(std::pair<unsigned int, unsigned int>(fragmentID,fragIndex));
@@ -36,50 +36,54 @@ DAQToOffline::tpcFragmentToRawDigits(art::EventID const& eid, artdaq::Fragments 
 
   if(debug){
     std::cout << eid
-	      << " has " << numFragments
-	      << " rawFragments" << std::endl;
+              << " has " << numFragments
+              << " rawFragments" << std::endl;
   }
-  
+
   std::vector<raw::RawDigit> rawDigitVector;
 
   //JPD -- first go at unpacking the information
-  //    -- seems to make sense to look through channel number, 
+  //    -- seems to make sense to look through channel number,
   //    -- then we'll create a rawDigit object for each channel
   //    -- will need some helper functions to do this for us, so I created a utilites directory
-  
+
 
   art::ServiceHandle<geo::Geometry> geometry;
   size_t numChans = geometry->Nchannels();
-  
+
   for(size_t chan=0;chan < numChans;chan++){
 
 
     //Each channel is uniquely identified by (fragmentID, group, sample) in an online event
-    
+
     unsigned int fragmentID = UnpackFragment::getFragIDForChan(chan);
     //unsigned int group = UnpackFragment::getNanoSliceGroupForChan(chan); <// No longer using group
     unsigned int sample = UnpackFragment::getNanoSliceSampleForChan(chan);
 
-    std::cout << "channel: " << chan
-	      << "\tfragment: " << fragmentID
+    if (debug) {
+      std::cout << "channel: " << chan
+                << "\tfragment: " << fragmentID
         //<< "\tgroup: " << group
-	      << "\tsample: " << sample
-	      << std::endl;
+                << "\tsample: " << sample
+                << std::endl;
+    }
 
     //Check that the necessary fragmentID is present in the event
     //i.e. do we have data for this channel?
 
     if( mapFragID.find(fragmentID) == mapFragID.end() ){
 
-      std::cout << "Fragment not found" << std::endl;
+      if (debug) std::cout << "Fragment not found" << std::endl;
       continue;
 
     }
 
     unsigned int fragIndex = mapFragID[fragmentID];
 
-    std::cout << "fragIndex: " << fragIndex << std::endl;
-    
+    if (debug) {
+      std::cout << "fragIndex: " << fragIndex << std::endl;
+    }
+
     std::vector<short> adcvec;
 
 
@@ -88,24 +92,26 @@ DAQToOffline::tpcFragmentToRawDigits(art::EventID const& eid, artdaq::Fragments 
 
     //Properties of fragment
     auto numMicroSlices = millisliceFragment.microSliceCount();
-    
+
     for(unsigned int i_micro=0;i_micro<numMicroSlices;i_micro++){
-     
+
       std::unique_ptr <const lbne::TpcMicroSlice> microSlice = millisliceFragment.microSlice(i_micro);
       auto numNanoSlices = microSlice->nanoSliceCount();
 
       for(uint32_t i_nano=0; i_nano < numNanoSlices; i_nano++){
-	std::unique_ptr<const lbne::TpcNanoSlice> nanoSlice = microSlice->nanoSlice(i_nano);
-	
-	uint16_t val = std::numeric_limits<uint16_t>::max();
-	bool success = nanoSlice->sampleValue(sample, val);
+        std::unique_ptr<const lbne::TpcNanoSlice> nanoSlice = microSlice->nanoSlice(i_nano);
 
-	if(success) adcvec.push_back(short(val));
-	
+        uint16_t val = std::numeric_limits<uint16_t>::max();
+        bool success = nanoSlice->sampleValue(sample, val);
+
+        if(success) adcvec.push_back(short(val));
+
       }
     }
 
-    std::cout << "adcvec->size(): " << adcvec.size() << std::endl;
+    if ( debug ) {
+      std::cout << "adcvec->size(): " << adcvec.size() << std::endl;
+    }
     unsigned int numTicks = adcvec.size();
     raw::Compress(adcvec, compression, zeroThreshold);
     raw::RawDigit theRawDigit(chan, numTicks, adcvec, compression);
