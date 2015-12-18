5ef67cf36d306e20aefd293c7742f09bc589eeec
Michael Baird <mbaird42@lbnefd03.fnal.gov>
2015-12-18 10:59:54 -0600

    mbaird42 - Adding a TTree header object that contains subrun summary info.

--- dune/NearlineMonitor/NearlineAna_module.cc
+++ dune/NearlineMonitor/NearlineAna_module.cc
@@ -20,6 +20,8 @@
 
 //ROOT
 #include "TH1I.h"
+#include "TTimeStamp.h"
+#include "TTree.h"
 
 //larsoft
 #include "Geometry/Geometry.h"
@@ -44,6 +46,7 @@ public:
   void printConfig();
   void analyze(art::Event const & e) override;
   void beginJob();
+  void endJob();
 
   //NoiseSpectrum plots
   void makeNoiseSpectrumPlots();
@@ -57,12 +60,47 @@ private:
   std::vector<TH1I*> fVecNoiseSpectrumPlots;
   art::InputTag fRawDigitsTag;
 
+  // Variables needed for the header info tree:
+  TTree*       fHeader;
+  unsigned int fRun;
+  unsigned int fSubrun;
+  int          fFirstEvent;
+  int          fLastEvent;
+  int          fNevents;
+  unsigned int fStartYear;
+  unsigned int fEndYear;
+  unsigned int fStartMonth;
+  unsigned int fEndMonth;
+  unsigned int fStartDay;
+  unsigned int fEndDay;
+  double       fStartHour;
+  double       fEndHour;
+  unsigned long long int fStartTime;
+  unsigned long long int fEndTime;
+
 };
+
 ////////////////////////////////////////////////////////////////////////////////
 
 nearline::NearlineAna::NearlineAna(fhicl::ParameterSet const & p)
   :
-  EDAnalyzer(p)  // ,
+  EDAnalyzer(p),
+  fHeader(0),
+  fRun(0),
+  fSubrun(0),
+  fFirstEvent(1e9),
+  fLastEvent(-1),
+  fNevents(0),
+  fStartYear(0),
+  fEndYear(0),
+  fStartMonth(0),
+  fEndMonth(0),
+  fStartDay(0),
+  fEndDay(0),
+  fStartHour(0.0),
+  fEndHour(0.0),
+  fStartTime(-1), // this is an unsigned int so it will default to a huge number
+  fEndTime(0)
 {
 
   {
@@ -71,6 +109,7 @@ nearline::NearlineAna::NearlineAna(fhicl::ParameterSet const & p)
   }
   reconfigure(p);
   printConfig();
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -109,7 +148,80 @@ void nearline::NearlineAna::beginJob(){
   mf::LogInfo logInfo("NearlineAna::beginJob");
   logInfo << "beginJob" << "\n";
 
+  // book the header object
+  art::ServiceHandle<art::TFileService> tfs;
+  fHeader = tfs->make<TTree>("Header","Subrun Information");
+  
+  fHeader->Branch("Run",&fRun);
+  fHeader->Branch("Subrun",&fSubrun);
+  fHeader->Branch("FirstEvent",&fFirstEvent);
+  fHeader->Branch("LastEvent",&fLastEvent);
+  fHeader->Branch("Nevents",&fNevents);
+  fHeader->Branch("StartYear",&fStartYear);
+  fHeader->Branch("StartMonth",&fStartMonth);
+  fHeader->Branch("StartDay",&fStartDay);
+  fHeader->Branch("StartHour",&fStartHour);
+  fHeader->Branch("EndYear",&fEndYear);
+  fHeader->Branch("EndMonth",&fEndMonth);
+  fHeader->Branch("EndDay",&fEndDay);
+  fHeader->Branch("EndHour",&fEndHour);
+
   if(fMakeNoiseSpectrumPlots) makeNoiseSpectrumPlots();
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void nearline::NearlineAna::endJob(){
+
+  //
+  // Compute header info.
+  //
+
+  //
+  // DISECTING the time from evt.time().value() into "human readable" format to display the date & time
+  //
+  unsigned int hour, minute, second;
+  int          nano;
+
+  // Get the time stamp.  art::Timestamp::value() returns a TimeValue_t which is a typedef to unsigned long long.
+  // The conventional use is for the upper 32 bits to have the seconds since 1970 epoch and the lower 32 bits to be
+  // the number of nanoseconds with the current second.
+  //
+  // NOTE: It seems that the above is NOT the convention for the 35t events. They only seem to use the lower 32 bits
+  //       for the year/month/day/hour/second. For now, I have reversed the values of lup and llo to get the time right.
+  //
+  //       THESE VARIABLES WILL NEED TO BE SWITCHED BACK IF USING THE LOWER 32 BITS FOR NANOSECONDS IS IMPLEMENTED!!!
+
+
+  const unsigned long int mask32 = 0xFFFFFFFFUL;
+
+  // taking start time apart
+
+  // unsigned long int lup = ( fStartTime >> 32 ) & mask32;
+  // unsigned long int llo = fStartTime & mask32;
+  unsigned long int llo = ( fStartTime >> 32 ) & mask32; // reversed value (see above comment)
+  unsigned long int lup = fStartTime & mask32;           // reversed value (see above comment)
+  TTimeStamp ts1(lup, (int)llo);
+  ts1.GetDate(kTRUE,0,&fStartYear,&fStartMonth,&fStartDay);
+  ts1.GetTime(kTRUE,0,&hour,&minute,&second);
+  nano = ts1.GetNanoSec();
+  double sec = ((double)second + (double)nano/1.0e9);
+  fStartHour = (double)hour + (double)minute/60.0 + sec/3600.0;
+
+  // taking end time apart
+  // lup = ( fEndTime >> 32 ) & mask32;
+  // llo = fEndTime & mask32;
+  llo = ( fEndTime >> 32 ) & mask32; // reversed value (see above comment)
+  lup = fEndTime & mask32;           // reversed value (see above comment)
+  TTimeStamp ts2(lup, (int)llo);
+  ts2.GetDate(kTRUE,0,&fEndYear,&fEndMonth,&fEndDay);
+  ts2.GetTime(kTRUE,0,&hour,&minute,&second);
+  nano = ts2.GetNanoSec();
+  sec = ((double)second + (double)nano/1.0e9);
+  fEndHour = (double)hour + (double)minute/60.0 + sec/3600.0;
+
+  fHeader->Fill();
 
 }
 
@@ -118,6 +230,29 @@ void nearline::NearlineAna::beginJob(){
 void nearline::NearlineAna::analyze(art::Event const & e)
 {
 
+  //
+  // Extract event info for the header...
+  //
+  unsigned int           run    = e.run();
+  unsigned int           subrun = e.subRun();
+  unsigned int           event  = e.id().event();
+  unsigned long long int time   = e.time().value();
+
+  fNevents++;
+  fRun    = run;
+  fSubrun = subrun;
+
+  // Don't assume first/last events are coorelated with start/end times...
+  if(time < fStartTime)        fStartTime = time;
+  if((int)event < fFirstEvent) fFirstEvent = event;
+  if(time > fEndTime)          fEndTime = time;
+  if((int)event > fLastEvent)  fLastEvent = event;
+
+
+
+  //
+  // Fill the desired plots.
+  //
   if(fMakeNoiseSpectrumPlots) fillNoiseSpectrumPlots(e);
 
 }
