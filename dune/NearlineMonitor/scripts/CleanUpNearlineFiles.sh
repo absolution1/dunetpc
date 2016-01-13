5e1695b0c13f24c4bfe1068721fab965eab1af45
LBNE DAQ <lbnedaq@lbne35t-gateway02.fnal.gov>
2016-01-13 11:49:08 -0600

    Limiting the number of events to 10, removal of the art output file, commenting IN the jobs in the crontab file.

--- dune/NearlineMonitor/scripts/CleanUpNearlineFiles.sh
+++ dune/NearlineMonitor/scripts/CleanUpNearlineFiles.sh
@@ -2,10 +2,10 @@
 
 outputDir=/lbne/data2/users/lbnedaq/nearline
 
-# No need to keep done or lock files after 90 days...
-find ${outputDir}/*/*/ -mtime +90 -name '*.DONE' -exec rm -v {} \;
-find ${outputDir}/*/*/ -mtime +90 -name '*.LOCK' -exec rm -v {} \;
+# (optional): Remove nearline output files.
+# find ${outputDir}/*/*/*/ -mtime +90 -name '*.DONE' -exec rm -v {} \;
+# find ${outputDir}/*/*/*/ -mtime +90 -name '*.LOCK' -exec rm -v {} \;
 
 # Delete stale output files...
-find ${outputDir}/*/*/ -mtime +1  -name 'TFileService*.root' -exec rm -v {} \;
-find /home/lbnedaq/nearline/temp -mtime +1 -name '*.root*' -exec rm -v {} \:
+find ${outputDir}/*/*/*/ -mtime +1 -name 'TFileService*.root' -exec rm -v {} \;
+find /home/lbnedaq/nearline/temp/ -mtime +1 -name '*.root*' -exec rm -v {} \:
