09863b3f01f8345ea236395d280eb296a584f245
Jonathan Davies <jpdavies@lbnegpvm04.fnal.gov>
2016-01-25 10:18:34 -0600

    Adding some possible useful data structures to the NearlinePlotMaker which will hopefully make the code more compact. These are not used yet, so the macro will work as it did before this commit

--- dune/NearlineMonitor/scripts/NearlinePlotMaker.h
+++ dune/NearlineMonitor/scripts/NearlinePlotMaker.h
@@ -52,5 +52,78 @@ UInt_t LastSR;
 
 
 
+struct NearlinePlot{
+  
+  TH1F* fHistogram;
+  TGraph* fGraphTime;
+  std::string fHistName;
+  int fNumPoints;
+  std::vector<float> fMetricVec;
+  std::vector<float> fTimeVec;
+  int fPlotCount;
+  bool fRMSPlot;
+
+  NearlinePlot(int Npoint, std::string this_hist_name, std::string hist_title, int num_bins, int min_x, int max_x, bool RMS=false);
+  bool AddHistogram(TFile const & file, TTree* header, int Xstrtime, int Xsrtime, int XNow, int GMToffset);
+  TCanvas* makeHistoCanvas(std::string can_name, std::string can_title, int width, int height, TPaveText* updateText);
+  TCanvas* makeGraphTimeCanvas(std::string can_name, std::string can_title, int width, int height);
+
+
+};
+
+
+NearlinePlot::NearlinePlot(int Npoint, std::string this_hist_name, std::string hist_title, int num_bins, int min_x, int max_x, bool RMS)
+{
+  fNumPoints = Npoint;
+  fHistName = this_hist_name;
+  fMetricVec = std::vector<float>(Npoint, 0);
+  fTimeVec = std::vector<float>(Npoint, 0);
+  fPlotCount = 0;
+  fRMSPlot=RMS;
+  fHistogram = new TH1F(fHistName.c_str(), hist_title.c_str(), num_bins, min_x, max_x);
+  fGraphTime = 0;
+}
+
+bool NearlinePlot::AddHistogram(TFile const & file, TTree* header, int Xstrtime, int Xsrtime, int XNow, int GMToffset){
+  TH1F *hist_temp = (TH1F*)file.FindObjectAny(fHistName.c_str());    
+  if(hist_temp != 0){
+    fHistogram->Add(hist_temp,1.0);
+  }    
+  else return false;
+
+  if(hist_temp != 0 && header != 0 && Xsrtime != XNow - GMToffset) {
+    fTimeVec.at(fPlotCount) = Xsrtime;
+    if(fRMSPlot) fMetricVec.at(fPlotCount) = hist_temp->GetRMS(1);
+    else fMetricVec.at(fPlotCount) = hist_temp->GetMean(1);
+    fPlotCount++;
+    return true;
+  }
+
+  return false;
+
+}
+
+TCanvas* NearlinePlot::makeHistoCanvas(std::string can_name, std::string can_title, int width, int height, TPaveText* updateText){
+  TCanvas *can = new TCanvas(can_name.c_str(), can_title.c_str(), width, height);
+  can->cd();
+  can->SetLogy();
+  gStyle->SetOptStat(111111);
+  fHistogram->SetLineWidth(2);
+  fHistogram->SetLineColor(kRed);
+  updateText->Draw();
+
+  return can;
+}
+
+TCanvas* NearlinePlot::makeGraphTimeCanvas(std::string can_name, std::string can_title, int width, int height){
+  TCanvas* can = new TCanvas(can_name.c_str(), can_title.c_str(), width, height);
+  can->cd();
+  gPad->SetGridx();
+  fGraphTime = new TGraph(fPlotCount);
+  for(int i=0;i<fPlotCount;i++) fGraphTime->SetPoint(i, fTimeVec.at(i), fMetricVec.at(i));
+  return can;
+  
+}
+
 
 #endif
