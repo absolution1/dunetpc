c3e2bc9021bb84748780dfb3a057181dafa9fc50
Michael Baird <mbaird42@dunegpvm07.fnal.gov>
2016-02-14 02:30:54 -0600

    The old lbne disk is now mounted as dune from the dunegpvms.

--- dune/NearlineMonitor/evd/run_RED35_evd.sh
+++ dune/NearlineMonitor/evd/run_RED35_evd.sh
@@ -24,8 +24,8 @@ export comp=e9
 export RelDir=/dune/app/home/duneana/35t_EventDisplay/larsoft_${version}
 export LPDir=${RelDir}/localProducts_larsoft_${version}_${comp}_prof
 export RED35Dir=/dune/app/home/duneana/35t_EventDisplay/RED35
-export InputPath=/lbne/data2/users/lbnedaq/nearline_evd/${version}
-export FileSearch='/lbne/data2/users/lbnedaq/nearline_evd/*/*/*/sample_done.root'
+export InputPath=/dune/data2/users/lbnedaq/nearline_evd/${version}
+export FileSearch='/dune/data2/users/lbnedaq/nearline_evd/*/*/*/sample_done.root'
 export filepos=9
 
 
