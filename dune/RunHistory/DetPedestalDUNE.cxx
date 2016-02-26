3055f180309a0a20f63386bf26a7be538ac24dcd
Jonathan Paley <jpaley@fnal.gov>
2016-02-26 12:46:33 -0600

    Add support for default pedestals per view in DUNE pedestal data provider.  Also cleaned up logic of handling CSV files vs. dB.
    Add slow controls data access RunHistory.  Once we know the channel names for things like purity, voltages, etc., we can provide specific methods to return this information.

--- dune/RunHistory/DetPedestalDUNE.cxx
+++ dune/RunHistory/DetPedestalDUNE.cxx
@@ -1,7 +1,7 @@
 #ifndef DETPEDESTALDUNE_CXX
 #define DETPEDESTALDUNE_CXX
 
-#include "DetPedestalDUNE.h"
+#include "dunetpc/dune/RunHistory/DetPedestalDUNE.h"
 #include <string>
 #include <iostream>
 #include "IFDatabase/Table.h"
@@ -20,7 +20,7 @@ namespace dune {
     fMeanErrMap.clear();
     fRmsErrMap.clear();
     fUseDB = false;
-    fUseDefaults = false;
+    fAbortIfNoPeds = false;
   }
 
   //------------------------------------------------------------
@@ -32,6 +32,7 @@ namespace dune {
     fMeanErrMap.clear();
     fRmsErrMap.clear();
     fUseDB = false;
+    fAbortIfNoPeds = false;
     fDetName = "";
     Configure(pset);
   }
@@ -44,38 +45,68 @@ namespace dune {
     if (!fUseDB) {
       fVldTime = p.get<bool>("Run",0);
     }
+    fAbortIfNoPeds = p.get<bool>("AbortIfNoPeds",false);
     fCSVFileName = p.get<std::string>("CSVFile","");    
     fUseDefaults = p.get<bool>("UseDefaults",false);
-    fDefaultMean = p.get<float>("DefaultMean",0.);
-    fDefaultMeanErr = p.get<float>("DefaultMeanErr",0.);
-    fDefaultRms = p.get<float>("DefaultRms",0.);
-    fDefaultRmsErr = p.get<float>("DefaultRmsErr",0.);    
+    fDefaultMean[geo::kU] = p.get<float>("DefaultMeanU",0.);
+    fDefaultMeanErr[geo::kU] = p.get<float>("DefaultMeanErrU",0.);
+    fDefaultRms[geo::kU] = p.get<float>("DefaultRmsU",0.);
+    fDefaultRmsErr[geo::kU] = p.get<float>("DefaultRmsErrU",0.);    
+    fDefaultMean[geo::kV] = p.get<float>("DefaultMeanV",0.);
+    fDefaultMeanErr[geo::kV] = p.get<float>("DefaultMeanErrV",0.);
+    fDefaultRms[geo::kV] = p.get<float>("DefaultRmsV",0.);
+    fDefaultRmsErr[geo::kV] = p.get<float>("DefaultRmsErrV",0.);    
+    fDefaultMean[geo::kZ] = p.get<float>("DefaultMeanZ",0.);
+    fDefaultMeanErr[geo::kZ] = p.get<float>("DefaultMeanErrZ",0.);
+    fDefaultRms[geo::kZ] = p.get<float>("DefaultRmsZ",0.);
+    fDefaultRmsErr[geo::kZ] = p.get<float>("DefaultRmsErrZ",0.);    
+    fDefaultMean[geo::kUnknown] = p.get<float>("DefaultMean",0.);
+    fDefaultMeanErr[geo::kUnknown] = p.get<float>("DefaultMeanErr",0.);
+    fDefaultRms[geo::kUnknown] = p.get<float>("DefaultRms",0.);
+    fDefaultRmsErr[geo::kUnknown] = p.get<float>("DefaultRmsErr",0.);    
+    
     fLogLevel = p.get<int>("LogLevel", 1);    
     if ( fLogLevel > 0 ) {
-      cout << myname << "          UseDB: " << fUseDB << endl;
-      cout << myname << "            Run: " << fVldTime << endl;
-      cout << myname << "        CSVFile: " << fCSVFileName << endl;
-      cout << myname << "    UseDefaults: " << fUseDefaults << endl;
-      cout << myname << "    DefaultMean: " << fDefaultMean << endl;
-      cout << myname << " DefaultMeanErr: " << fDefaultMeanErr << endl;
-      cout << myname << "     DefaultRms: " << fDefaultRms << endl;
-      cout << myname << "  DefaultRmsErr: " << fDefaultRmsErr << endl;
-      cout << myname << "       LogLevel: " << fLogLevel << endl;
+      std::cout << myname << "            UseDB: " << fUseDB << std::endl;
+      std::cout << myname << "              Run: " << fVldTime << std::endl;
+      std::cout << myname << "          CSVFile: " << fCSVFileName << std::endl;
+      std::cout << myname << "      UseDefaults: " << fUseDefaults << std::endl;
+      std::cout << myname << "   DefaultMean[U]: " << fDefaultMean[geo::kU] << std::endl;
+      std::cout << myname << "DefaultMeanErr[U]: " << fDefaultMeanErr[geo::kU] << std::endl;
+      std::cout << myname << "    DefaultRms[U]: " << fDefaultRms[geo::kU] << std::endl;
+      std::cout << myname << " DefaultRmsErr[U]: " << fDefaultRmsErr[geo::kU] << std::endl;
+      std::cout << myname << "   DefaultMean[V]: " << fDefaultMean[geo::kV] << std::endl;
+      std::cout << myname << "DefaultMeanErr[V]: " << fDefaultMeanErr[geo::kV] << std::endl;
+      std::cout << myname << "    DefaultRms[V]: " << fDefaultRms[geo::kV] << std::endl;
+      std::cout << myname << " DefaultRmsErr[V]: " << fDefaultRmsErr[geo::kV] << std::endl;
+      std::cout << myname << "   DefaultMean[Z]: " << fDefaultMean[geo::kZ] << std::endl;
+      std::cout << myname << "DefaultMeanErr[Z]: " << fDefaultMeanErr[geo::kZ] << std::endl;
+      std::cout << myname << "    DefaultRms[Z]: " << fDefaultRms[geo::kZ] << std::endl;
+      std::cout << myname << " DefaultRmsErr[Z]: " << fDefaultRmsErr[geo::kZ] << std::endl;
+      std::cout << myname << "         LogLevel: " << fLogLevel << std::endl;
     }
+
     return true;
   }
 
   //------------------------------------------------------------
 
-  bool DetPedestalDUNE::Update(uint64_t ts) {
-    const string myname = "DetPedestalDUNE::Update: ";
-    if ( fLogLevel > 0 ) {
-      cout << myname << "Called with run " << ts << endl;
-    }
+  bool DetPedestalDUNE::Update(uint64_t ts)
+  {
+    if (fUseDefaults) return true;
+
+    if ( fLogLevel > 0 ) 
+      std::cout << __FUNCTION__ << " Called with run " << ts << std::endl;
+
     if (!fUseDB && fCSVFileName.empty()) {
-      cout << myname << "Both UseDB is false and CSVFileName isn't set, so pedestals retrieval would fail..Leaving update." << std::endl;
-      return true;
+      std::cout << __FUNCTION__ << " Method of determining pedestals is undefined! Either set UseDB or CSVFileName in fhicl. ";
+      if (fAbortIfNoPeds) {
+	std::cout << "Aborting per request." << std::endl;
+	abort();
+      }
+      return false;
     }
+
     if (ts == fVldTime)
       return true;
     else
@@ -108,14 +139,18 @@ namespace dune {
     t.SetMaxTSVld(ts);
 
     t.SetVerbosity(100);
-    if (fCSVFileName.empty())
+    if (fUseDB)
       t.Load();
-    else
+    else 
       t.LoadFromCSV(fCSVFileName);
-
+    
     if (t.NRow() == 0) {
-      std::cerr << "Number of pedestals from database/CSV file is 0.  This should never be the case, aborting." << std::endl;
-      abort();
+      std::cout << "Number of pedestals from database/CSV file is 0.  This should never be the case!  ";
+      if (fAbortIfNoPeds) {
+	std::cout << "Aborting, per request." << std::endl;
+	abort();
+      }
+      return false;
     }
 
     nutools::dbi::Row* row;
@@ -141,16 +176,67 @@ namespace dune {
   
   //------------------------------------------------------------
 
-  float DetPedestalDUNE::PedMean(raw::ChannelID_t ch) const {
+  float DetPedestalDUNE::DefaultMean(geo::View_t v) const {
+    float retVal=-1.;
+    auto it = fDefaultMean.find(v);
+    if (it != fDefaultMean.end())
+      return it->second;
+    else
+      return retVal;
+
+  }
+  
+  //------------------------------------------------------------
+
+  float DetPedestalDUNE::DefaultMeanErr(geo::View_t v) const {
+    float retVal=-1.;
+    auto it = fDefaultMeanErr.find(v);
+    if (it != fDefaultMeanErr.end())
+      return it->second;
+    else
+      return retVal;
+
+  }
+  
+  //------------------------------------------------------------
+
+  float DetPedestalDUNE::DefaultRms(geo::View_t v) const {
+    float retVal=-1.;
+    auto it = fDefaultRms.find(v);
+    if (it != fDefaultRms.end())
+      return it->second;
+    else
+      return retVal;
+
+  }
+  
+  //------------------------------------------------------------
+
+  float DetPedestalDUNE::DefaultRmsErr(geo::View_t v) const {
+    float retVal=-1.;
+    auto it = fDefaultRmsErr.find(v);
+    if (it != fDefaultRmsErr.end())
+      return it->second;
+    else
+      return retVal;
+
+  }
+  
+  //------------------------------------------------------------
+
+  float DetPedestalDUNE::PedMean(raw::ChannelID_t ch) const { 
+    float retVal=-1.;
     if (fUseDefaults) {
-      return fDefaultMean;
+      auto it = fDefaultMean.find(geo::kUnknown);
+      if (it != fDefaultMean.end())
+	return it->second;
+      else
+	return retVal;
     }
     if (fVldTime == 0) {
       std::cerr << "DetPedestalDUNE: Validity time is not set!  Aborting." << std::endl;
       abort();
     }
-
-    float retVal=-1.;
     auto it = fMeanMap.find(ch);
     if (it != fMeanMap.end())
       retVal = it->second;
@@ -161,13 +247,19 @@ namespace dune {
   //------------------------------------------------------------
 
   float DetPedestalDUNE::PedMeanErr(raw::ChannelID_t ch) const { 
-    if (fUseDefaults)
-      return fDefaultMeanErr;
+    float retVal=0.;
+    if (fUseDefaults) {
+      auto it = fDefaultMeanErr.find(geo::kUnknown);
+      if (it != fDefaultMeanErr.end())
+	return it->second;
+      else
+	return retVal;
+    }
+
     if (fVldTime == 0) {
       std::cerr << "DetPedestalDUNE: Validity time is not set!  Aborting." << std::endl;
       abort();
     }
-    float retVal=0.;
     auto it = fMeanErrMap.find(ch);
     if (it != fMeanErrMap.end())
       retVal = it->second;
@@ -178,13 +270,18 @@ namespace dune {
     //------------------------------------------------------------
 
   float DetPedestalDUNE::PedRms(raw::ChannelID_t ch) const { 
-    if (fUseDefaults)
-      return fDefaultRms;
+    float retVal=-1;
+    if (fUseDefaults) {
+      auto it = fDefaultRms.find(geo::kUnknown);
+      if (it != fDefaultRms.end())
+	return it->second;
+      else
+	return retVal;
+    }
     if (fVldTime == 0) {
       std::cerr << "DetPedestalDUNE: Validity time is not set!  Aborting." << std::endl;
       abort();
     }
-    float retVal=0.;
     auto it = fRmsMap.find(ch);
     if (it != fRmsMap.end())
       retVal = it->second;
@@ -195,13 +292,18 @@ namespace dune {
   //------------------------------------------------------------
 
   float DetPedestalDUNE::PedRmsErr(raw::ChannelID_t ch) const { 
-    if (fUseDefaults)
-      return fDefaultRmsErr;
+    float retVal=0.;
+    if (fUseDefaults) {
+      auto it = fDefaultRmsErr.find(geo::kUnknown);
+      if (it != fDefaultRmsErr.end())
+	return it->second;
+      else
+	return retVal;
+    }
     if (fVldTime == 0) {
       std::cerr << "DetPedestalDUNE: Validity time is not set!  Aborting." << std::endl;
       abort();
     }
-    float retVal=0.;
     auto it = fRmsErrMap.find(ch);
     if (it != fRmsErrMap.end())
       retVal = it->second;
