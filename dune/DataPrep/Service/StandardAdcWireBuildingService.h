4de0773fcffeff7fb26710dac396c10e65c5ffbe
David Adams <dladams@bnl.gov>
2016-07-20 14:21:24 -0400

    Add StandardAdcWireBuildingService.

---
+++ dune/DataPrep/Service/StandardAdcWireBuildingService.h
@@ -0,0 +1,37 @@
+// StandardAdcWireBuildingService.h
+//
+// David Adams
+// June 2016
+//
+// Implementation of service to build the recob::Wire in AdcChannelData.
+//
+// Configuration:
+//   LogLevel    - usual log level
+//
+#ifndef StandardAdcWireBuildingService_H
+#define StandardAdcWireBuildingService_H
+
+#include "dune/DuneInterface/AdcWireBuildingService.h"
+
+class AdcSuppressService;
+
+class StandardAdcWireBuildingService : public AdcWireBuildingService {
+
+public:
+
+  StandardAdcWireBuildingService(fhicl::ParameterSet const& pset, art::ActivityRegistry&);
+
+  int build(AdcChannelData& data, WireVector* pwires) const;
+
+  std::ostream& print(std::ostream& out =std::cout, std::string prefix ="") const;
+
+private:
+
+  // Parameters.
+  int m_LogLevel;
+
+};
+
+DECLARE_ART_SERVICE_INTERFACE_IMPL(StandardAdcWireBuildingService, AdcWireBuildingService, LEGACY)
+
+#endif
