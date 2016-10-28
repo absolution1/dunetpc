92875bcde77e29dc482b402e0e684d7e2cd41521
Robert Sulej <robert.sulej@cern.ch>
2016-10-28 09:30:34 -0500

    added simple access to data products and histogram filling

--- dune/Protodune/TutorialExamples/ClusterCounter_module.cc
+++ dune/Protodune/TutorialExamples/ClusterCounter_module.cc
@@ -13,9 +13,14 @@
 #include "art/Framework/Principal/Handle.h"
 #include "art/Framework/Principal/Run.h"
 #include "art/Framework/Principal/SubRun.h"
+#include "art/Framework/Services/Optional/TFileService.h" 
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include "lardataobj/RecoBase/Cluster.h"
+
+#include "TTree.h"
 
 namespace tutorial {
 
@@ -32,14 +37,68 @@ public:
   // Required functions.
   void analyze(art::Event const & e) override;
 
+  // Selected optional functions.  
+  void beginJob() override;
+  void endJob() override;
+
 private:
+  size_t fEvNumber;
+
+  TTree *fEventTree;
+  size_t fNClusters;
+
+  TTree *fClusterTree;
+  size_t fNHits;
+
+  // ******* fcl parameters *******
+  art::InputTag fClusterModuleLabel;
+  size_t fMinSize;
 };
 
-ClusterCounter::ClusterCounter(fhicl::ParameterSet const & p) : EDAnalyzer(p) {}
-
-void ClusterCounter::analyze(art::Event const & e)
+ClusterCounter::ClusterCounter(fhicl::ParameterSet const & p) : EDAnalyzer(p)
 {
-    std::cout << "ClusterCounter module on event #" << e.id().event() << std::endl;
+    fClusterModuleLabel = p.get< std::string >("ClusterModuleLabel");
+    fMinSize = p.get< size_t >("MinSize");
+}
+
+void ClusterCounter::beginJob()
+{
+    art::ServiceHandle<art::TFileService> tfs; // TTree's are created in the memory managed by ROOT (you don't delete them)
+
+    fEventTree = tfs->make<TTree>("EventTree", "event by event info");
+    fEventTree->Branch("event", &fEvNumber, "fEvNumber/I");
+    fEventTree->Branch("nclusters", &fNClusters, "fNClusters/I");
+
+    fClusterTree = tfs->make<TTree>("ClusterTree", "cluster by cluster info");
+    fClusterTree->Branch("event", &fEvNumber, "fEvNumber/I");
+    fClusterTree->Branch("nhits", &fNHits, "fNHits/I");
+}
+
+void ClusterCounter::analyze(art::Event const & evt)
+{
+    fEvNumber = evt.id().event();
+    mf::LogVerbatim("ClusterCounter") << "ClusterCounter module on event #" << fEvNumber;
+
+    // use auto to make the line shorter when you remember all art types,
+    // the type here is: art::ValidHandle< std::vector<recob::Cluster> >
+    auto clusterHandle = evt.getValidHandle< std::vector<recob::Cluster> >(fClusterModuleLabel);
+    
+    // if you are old c++ granpa (or need cluster index, which may indeed happen!):
+    // for (size_t i = 0; i < clusterHandle->size(); ++i)
+    // or:
+    for (auto const & clu : *clusterHandle) // loop over recob::Cluster's stored in the std::vector 
+    {
+        fNHits = clu.NHits();
+        fClusterTree->Fill();
+    }
+
+    fNClusters = clusterHandle->size();
+    fEventTree->Fill();
+}
+
+void ClusterCounter::endJob()
+{
+    mf::LogVerbatim("ClusterCounter") << "ClusterCounter finished job";
 }
 
 } // tutorial namespace
