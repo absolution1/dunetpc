7dc5904419b7d1cb84bc422a3204f106c11eaa3b
David Adams <dladams@bnl.gov>
2017-08-22 13:12:32 -0400

    Add tool AdcPedestalFitter.

---
+++ dune/DataPrep/Tool/AdcPedestalFitter.h
@@ -0,0 +1,60 @@
+// AdcPedestalFitter.h
+
+// David Adams
+// August 2017
+//
+// Tool to fit ADC data and extract a pedestal.
+//
+// Configuration:
+//   LogLevel - 0=silent, 1=init, 2=each event, >2=more
+//   HistName:  Name for the histogram.
+//   HistTitle: Title for the histogram.
+//   HistManager: Name of the tool that manages the histogram.
+//                Right now this is the only way to retrive the fit result.
+// The following subsitutions are made in the names:
+//    %RUN% - run number
+//    %SUBRUN% - event number
+//    %EVENT% - event number
+//    %CHAN% - channel number
+
+#ifndef AdcPedestalFitter_H
+#define AdcPedestalFitter_H
+
+#include "art/Utilities/ToolMacros.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "dune/DuneInterface/Tool/AdcChannelViewer.h"
+#include <string>
+#include <vector>
+
+class HistogramManager;
+
+class AdcPedestalFitter : AdcChannelViewer {
+
+public:
+
+  AdcPedestalFitter(fhicl::ParameterSet const& ps);
+
+  int view(const AdcChannelData& acd) const override;
+
+private:
+
+  using Name = std::string;
+  using NameVector = std::vector<Name>;
+
+  // Configuration data.
+  int m_LogLevel;
+  Name m_HistName;
+  Name m_HistTitle;
+  Name m_HistManager;
+
+  // Histogram manager.
+  HistogramManager* m_phm;
+
+  // Make replacements in a name.
+  Name nameReplace(Name name, const AdcChannelData& acd) const;
+
+};
+
+DEFINE_ART_CLASS_TOOL(AdcPedestalFitter)
+
+#endif
