9f337cd9312e5ac24f1e7e542f67922fd11ef673
Jonathan Paley <jpaley@fnal.gov>
2017-10-14 23:37:27 -0500

    New BeamData package to hold beam data provider classes that do experiment-specific extraction of relevant beam data.  Including protoDUNE class, still under development.

---
+++ dune/BeamData/protoDUNEBeam.cxx
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////////////////////
+//
+//  protoDUNEBeam
+//
+//  jpaley@fnal.gov
+//
+////////////////////////////////////////////////////////////////////////
+// Framework includes
+
+// C++ language includes
+#include <iostream>
+
+// LArSoft includes
+#include "protoDUNEBeam.h"
+
+// Framework includes
+#include "messagefacility/MessageLogger/MessageLogger.h"
+#include "cetlib/exception.h"
+
+//#include "nutools/IFDatabase/Util.h"
+//#include <boost/tokenizer.hpp>
+//#include <ctime>
+
+namespace proto {
+  //-----------------------------------------------
+  ProtoDUNEBeam::ProtoDUNEBeam(fhicl::ParameterSet const& pset, ifbeam_ns::BeamFolder* ptr) :
+    _loadFromDB   (pset.get<bool        >("LoadFromDB")),
+    _timeWindow   (pset.get<double      >("TimeWindow")),
+    _csvFileName  (pset.get<std::string >("CSVFileName")),
+    _bundleName   (pset.get<std::string >("BundleName")),
+    _bfp(ptr)
+  {
+
+  }
+  
+  //------------------------------------------------
+  bool ProtoDUNEBeam::Update(uint64_t ts) 
+  {
+    if (_currentTS == ts) return true;
+
+    //    double trtgtd=0.;
+
+    _xy1.clear();
+    _xy2.clear();
+    _xy3.clear();
+    _xy4.clear();
+    _ckov1.clear();
+    _ckov2.clear();
+    _xy1 = _bfp->GetNamedVector(ts,"E:NT04XY1[]");
+    _xy2 = _bfp->GetNamedVector(ts,"E:NT04XY2[]");
+    _xy3 = _bfp->GetNamedVector(ts,"E:NT04XY3[]");
+    _xy4 = _bfp->GetNamedVector(ts,"E:NT04XY4[]");
+    _ckov1 = _bfp->GetNamedVector(ts,"E:NT04Ckov1[]");
+    _ckov2 = _bfp->GetNamedVector(ts,"E:NT04Ckov2[]");
+
+    return true;
+  }
+
+}
---
+++ dune/BeamData/protoDUNEBeam.h
@@ -0,0 +1,61 @@
+////////////////////////////////////////////////////////////////////////
+//  protoDUNEBeam.h
+//
+//  Data provider class for beam data
+//
+// jpaley@fnal.gov
+//
+////////////////////////////////////////////////////////////////////////
+#ifndef PROTODUNEBEAM_H
+#define PROTODUNEBEAM_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <memory>
+
+#include "fhiclcpp/ParameterSet.h"
+#include "ifbeam.h"
+
+//#include "lardata/DetectorInfo/RunHistory.h"
+//#include "nutools/IFDatabase/Table.h"
+
+///General LArSoft Utilities
+namespace proto {
+
+  class ProtoDUNEBeam  {
+  public: 
+    ProtoDUNEBeam(fhicl::ParameterSet const& pset, ifbeam_ns::BeamFolder* ptr);
+    ~ProtoDUNEBeam() {};
+
+    bool Update(uint64_t ts = 0);
+
+    void SetLoadFromDB(bool v) {_loadFromDB = v;}
+    void SetCSVFileName(std::string s) {_csvFileName=s;}
+    void SetTimeWindow(int tw) {_timeWindow = tw;}
+    void SetBundleName(std::string bn) {_bundleName = bn; }
+    bool LoadBeamFolder();
+    void LoadData();
+    
+  private:
+    
+    bool        _loadFromDB;
+    double      _timeWindow; // time window in seconds to search for data
+    std::vector<double> _xy1; 
+    std::vector<double> _xy2; 
+    std::vector<double> _xy3;
+    std::vector<double> _xy4; 
+    std::vector<double> _ckov1; 
+    std::vector<double> _ckov2; 
+ 
+    uint64_t    _currentTS;
+    std::string _csvFileName;
+    std::string _bundleName;
+
+    std::unique_ptr<ifbeam_ns::BeamFolder> _bfp;
+    
+  }; // class ProtoDUNEBeam
+} //namespace dune
+#endif // RUNHISTORYDUNE_H
+
