953196841846d90b3d6b59cad7a7720212ee0bdf
Jason Stock <jstock@dunegpvm09.fnal.gov>
2018-02-19 14:11:42 -0600

    Update to CalibrationTree. Starting debugging.

--- dune/DAQSimAna/CalibrationTree.h
+++ dune/DAQSimAna/CalibrationTree.h
@@ -48,7 +48,8 @@ namespace CalibrationTree {
   class CalibrationTree : public art::EDAnalyzer
   {
     public:
-
+      
+      //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
       struct fhiclConfig{
         fhicl::Atom<art::InputTag> HitLabel{
           fhicl::Name("HitLabel"),
@@ -67,23 +68,122 @@ namespace CalibrationTree {
         };//end TrackLabel
       };//end fhiclConfig
 
+      //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      struct jointHitRecord{ //To be used in a map with Particles
+        const simb::MCParticle* particle;
+        //const simb::MCParticle* eve;
+        int eveIndex;
+        std::vector<std::tuple<art::Ptr<recob::Hit>, unsigned int, double>> hitsRec;
+        std::vector<std::tuple<art::Ptr<recob::OpHit>, unsigned int, double>> ohitsRec;
+        jointHitRecord::jointHitRecord(simb::MCParticle* part )
+          :particle(part)
+        {}
+        void AddHit(art::Ptr<recob::Hit> hit, unsigned int hitIndex, double split=1.0 ){ 
+          hitsRec.push_back(std::make_tuple (hit, hitIndex, split)); 
+        }
+        void AddHit(art::Ptr<recob::OpHit> ophit, unsigned int hitIndex, double split=1.0){
+          ohitsRec.push_back(std::make_tuple (ophit, hitIndex, split));
+        }
+        std::vector<std::tuple<art::Ptr<recob::Hit>, unsigned int, double>> const& GetHits(){ return hitsRec; }
+        std::vector<std::tuple<art::Ptr<recob::OpHit>, unsigned int, double>> const& GetOpHits(){ return ohitsRec;}
+        void clear(){
+          hitsRec.clear();
+          ohitsRec.clear();
+        }
+        bool operator<(const jointHitRecord& rhs) const{ return particle<rhs.particle; }
+        bool operator>(const jointHitRecord& rhs) const{ return rhs<this; }
+        bool operator<=(const jointHitRecord& rhs) const{ return !(this>rhs);}
+        bool operator>=(const jointHitRecord& rhs) const{ return !(this<rhs);}
+        bool operator==(const jointHitRecord& rhs) const{ return particle==rhs.particle;}
+        bool operator!=(const jointHitRecords& rhs) const{ return ! this==rhs; }
+      };
+
+      //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      struct particleBuffer{ 
+        int pnum;
+        int pid;
+        int eveIndex;
+        int event;
+        int subrun;
+        int run;
+        //buffer constructor
+        particleBuffer()
+          :pnum(0), pid(0), eveIndex(0), event(0), subrun(0), run(0)
+        { }
+        particleBuffer(int mpnum, int mpid, int meveIndex, int mevent, int msubrun, int mrun)
+          :pnum(mpnum), pid(mpid), eveIndex(meveIndex), event(mevent), subrun(msubrun), run(mrun)
+        { }
+        //buffer update
+        update(int mpnum, int mpid, int meveIndex, int mevent, int msubrun, int mrun)
+          :pnum(mpnum), pid(mpid), eveIndex(meveIndex), event(mevent), subrun(msubrun), run(mrun)
+        { }
+      };
+
+      //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      struct hitBuffer{
+        double charge;
+        double time;
+        double width;
+        double split;
+        int wire;
+        int hitIndex;
+        int pnum;
+        hitBuffer()
+          :charge(0),time(0), width(0), split(0), wire(0), hitIndex(0), pnum(0)
+        { }
+        hitBuffer(double mcharge, double mtime, double mwidth, double msplit, int mwire, int mhitIndex, mpnum)
+          :charge(mcharge), time(mtime), width(mwidth), split(msplit), wire(mwire), hitIndex(mhitIndex), pnum(mpnum)
+        {}
+        update(double mcharge, double mtime, double mwidth, double msplit, int mwire, int mhitIndex, int mpnum)
+          :charge(mcharge), time(mtime), width(mwidth), split(msplit), wire(mwire), hitIndex(mhitIndex), pnum(mpnum)
+        { }
+      };
+
+      //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      struct lightBuffer{
+        double PEs;
+        double time;
+        double width;
+        int opChannel;
+        int opHitIndex;
+        lightBuffer()
+          :PEs(0),time(0),width(0), split(0),opChannel(0), opDet(0), opHitIndex(0), pnum(0)
+        {}
+        lightBuffer(double mpes, double mtime, double mwidth, double msplit, int mopchan,  int mopindex, int mpnum)
+          :PEs(mpes), time(mtime), width(mwidth), split(msplit), opChannel(mopchan),  opHitIndex(mopindex), pnum(mpnum)
+        {}
+        update(double mpes, double mtime, double mwidth, double msplit, int mopchan,  int mopindex, int mpnum)
+          :PEs(mpes), time(mtime), width(mwidth), split(msplit), opChannel(mopchan),  opHitIndex(mopindex), pnum(mpnum)
+        {}
+      };
+
       explicit CalibrationTree(fhicl::ParameterSet const& parameterSet);
-      explicit CalibrationTree(fhiclConfig& config);
+      explicit CalibrationTree(fhiclConfig const& config);
 
       virtual void beginJob();
       virtual void analyse(const art::Event& evt) override;
       virtual void endJob();
 
     private:
-      
+
+      std::vector<jointHitRecord> jhrV;
+
+      void FillJHRV(int trackId, art::Ptr<recob::Hit> hit, double split=1.0);
+      void FillJHRV(int trackId, art::Ptr<recob::OpHit> hit, double split=1.0);
+
+      art::ServiceHandle<art::TFileService> private_service_tfs;
       TTree* private_CalibrationTree;
       TBranch* private_CalibrationParticle;
       TBranch* private_CalibrationCharge;
       TBranch* private_CalibrationLight;
 
+      particleBuffer private_particleBuffer;
+      hitBuffer      private_hitBuffer;
+      opticalBuffer  private_lightBuffer;
+
       //Also need buffers
 
-  }// end class CalibrationTree
+  };// end class CalibrationTree
 
 }
 
--- dune/DAQSimAna/CalibrationTree_module.cc
+++ dune/DAQSimAna/CalibrationTree_module.cc
@@ -4,3 +4,183 @@
  * \brief: This is a small analysis Tree made for use in the Calibration group to investigate radiological backgrounds and calibration sources.
  *
  */
+
+namespace{}
+
+namespace CalibrationTree {
+
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  CalibrationTree::CalibrationTree(fhichl::ParameterSet const& pSet)
+    :priv_HitLabel(pSet.get<art::InputTag>("HitLabel","gausshit")),
+    priv_OpHitLabel(pSet.get<art::InputTag>("OpHitLabel","ophit"))
+  {  }
+
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  CalibrationTree::CalibrationTree(fhiclcConfig const& config)
+    :priv_HitLabel(config.HitLabel()),
+    priv_OpHitLabel(config.OpHitLabel())
+  {  }
+
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  void FillJHRV(int trackId, art::Ptr<recob::Hit> hit, int hitIndex, double split){ //Factor out everything that doesn't require hit (all the trackID stuff) into it's own function (as it is duplicated in FillJHRV for ophits) or turn this into a template.
+    simb::MCParticle* tIdToPart = PIS->TrackIdToParticle(trackId);
+    simb::MCParticle* eve       = PIS->TrackIdToParticle(PIS->TrackIdToEveId(trackId));
+    bool findPart = [](auto& a){ return a.particle==tIdToPart;}
+    bool findEve  = [](auto& a){ return a.particle==eve;}
+    std::vector<jointHitRecord>::iterator partRec = std::find_if(jhrV.begin(), jhrV.end(), findPart);
+    std::vector<jointHitRecord>::iterator eveRec = std::find_if(jhrV.begin(), jhrV.end(), findPart);
+    auto epos = std::distance(jhrV.begin(),eveRec); 
+    if(partRec==jhrV.end()){ //make new Entry
+      if(eveRec==jhrV.end()){ //Eve must also be added.
+        auto itr = jhrV.emplace_back(tIdToPart);
+        itr->eveIndex=jhrV.size();
+        jhrV.emplace_back(eve);
+      }else{//Eve exists, part does not. Make part, assign correct eve.
+        auto itr = jhrV.emplace_back(tIdToPart);
+        itr->eveIndex=epos;
+      }
+    }else{//Part exists. Does part know it has an eve? If no, tell it where it's eve lives.
+      if( !(partRec->eveIndex) && (eveRec!=jhrV.end() ) ){
+        partRec->eveIndex = epos;
+      }else if( !(partRec->eveIndex) && (eveRec!=jhrV.end() ) ){ 
+        partRec->eveIndex=jhrV.size();
+        jhrV.emplace_back(eve);
+      }else{//particle exists and knows it's eve
+        //Only care if it doesn't agree with the eve we just found.
+        if(partRec->eveIndex!=epos){throw;}
+      }
+    }
+    //Do the actual filling.
+    partRec = std::find_if(jhrV.begin(), jhrV.end(), findPart);//update partRec (in case we added the particle ourselves.
+    if(partRec==jhrV.end()){throw;} //This should never happen by this point, as I explicitly added it if it didn't exist.
+    partRec->AddHit(hit, split);
+  }
+
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  void FillJHRV(int trackId, art::Ptr<recob::OpHit> hit, double ophitIndex, double split){
+    simb::MCParticle* tIdToPart = PIS->TrackIdToParticle(trackId);
+    simb::MCParticle* eve       = PIS->TrackIdToParticle(PIS->TrackIdToEveId(trackId));
+    bool findPart = [](auto& a){ return a.particle==tIdToPart;}
+    bool findEve  = [](auto& a){ return a.particle==eve;}
+    std::vector<jointHitRecord>::iterator partRec = std::find_if(jhrV.begin(), jhrV.end(), findPart);
+    std::vector<jointHitRecord>::iterator eveRec = std::find_if(jhrV.begin(), jhrV.end(), findPart);
+    auto epos = std::distance(jhrV.begin(),eveRec); 
+    if(partRec==jhrV.end()){ //make new Entry
+      if(eveRec==jhrV.end()){ //Eve must also be added.
+        auto itr = jhrV.emplace_back(tIdToPart);
+        itr->eveIndex=jhrV.size();
+        jhrV.emplace_back(eve);
+      }else{//Eve exists, part does not. Make part, assign correct eve.
+        auto itr = jhrV.emplace_back(tIdToPart);
+        itr->eveIndex=epos;
+      }
+    }else{//Part exists. Does part know it has an eve? If no, tell it where it's eve lives.
+      if( !(partRec->eveIndex) && (eveRec!=jhrV.end() ) ){
+        partRec->eveIndex = epos;
+      }else if( !(partRec->eveIndex) && (eveRec!=jhrV.end() ) ){ 
+        partRec->eveIndex=jhrV.size();
+        jhrV.emplace_back(eve);
+      }else{//particle exists and knows it's eve
+        //Only care if it doesn't agree with the eve we just found.
+        if(partRec->eveIndex!=epos){throw;}
+      }
+    }
+    //Do the actual filling.
+    partRec = std::find_if(jhrV.begin(), jhrV.end(), findPart);//update partRec (in case we added the particle ourselves.
+    if(partRec==jhrV.end()){throw;} //This should never happen by this point, as I explicitly added it if it didn't exist.
+    partRec->AddHit(hit, split);
+  }
+
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  virtual void beginJob(){
+    private_CalibrationTree = private_service_tfs->make<TTree>("CalibrationTree", "A TTree for short records of the hits and ophits produced by given particles in the simulation");
+    private_CalibrationParticle = private_CalibrationTree->Branch("particles", &private_particleBuffer, "pid/i:eveIndex/i:event/i:subrun/i:run/i");
+    private_CalibrationCharge = private_CalibrationParticle->Branch("hits",&private_hitBuffer,"charge/d:time/d:width/d:wire/i:hitIndex/i");
+    private_CalibrationLight = private_CalibrationParticle->Branch("ophits", &private_lightBuffer, "pes/d:time/d:width/d:opchan/i:opdet/i:opHitIndex/i");
+  }
+
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  virtual void analyse(const art::Event& evt) override{
+    //Set up BackTracking
+    art::ServiceHandle<cheat::ParticleInventoryService> PIS;
+    art::ServiceHandle<cheat::BackTrackerService> BTS;
+    art::ServiceHandle<cheat::PhotonBackTrackerService> PBS;
+
+    art::Handle<std::vector<recob::Hit>> hitHandle;
+    std::vector<art::Ptr<recob::Hit>> hitList;
+    if(evt.getByLabel(private_HitLabel,hitHandle) )
+      art::fill_ptr_vector(hitList, hitHandle);
+    art::Handle<std::vector<recob::OpHit>> opHitHandle;
+    std::vector<art::Ptr<recob::OpHit>> opHitList;
+    if(evt.getByLabel(private_OpHitLabel,opHitHandle) )
+      art::fill_ptr_vector(opHitList, opHitHandle);
+
+    unsigned int hitCounter=0;
+    for ( auto const& hit : hitList){
+      hitCounter++;
+      std::vector<const sim::IDE*> IDEs_Ps 
+        = BTS->HitToSimIDEs_Ps(hit);
+      if(IDEs_Ps.empty()){
+        //I don't know what this would be. Maybe make a dummy particle? A dummy jhr?
+        //I am leaning toward a dummy JHR that just holds all of the hits that aren't from tracks. This singleton doesn't need to be in the map, but can be handeled as a special case. If I do want it in the map, I have to make a dummy MCParticle.
+        //That way nothing is lost and noise studies are possible.
+      }else{//This needs to work more like TrackIDEs, but really it needs to be particleIDEs, with appropriate particle energy fractions. Perhaps I should add this to the BackTracker (can't add to SimChannels. Too much work, breaking change. But doing it in the backtracker should be fine.
+        for( auto const& ide_P : IDEs_Ps){
+          //This can still be a fake hit. Include a check for an unbelievably low amount of energy deposited (if the hit is noise but happened to corespond to a stray electron that did in fact hit the wires).
+          //I chose to scale by nelectrons, this can be changed later by flipping which line is commented below.
+          int trackId = ide_P->trackID;
+          double split = ide_P->numElectrons;
+          this->FillJHRV(trackId, hit, hitCounter, split);
+        }//end for ide_P in IDEs_Ps
+      }//end IDEs_Ps not empty
+    }//end for hit in hitlist
+
+
+    hitCounter=0;
+    for ( auto const& ophit : opHitList ){
+      ++hitCounter;
+      std::vector<const sim::SDP*> SDPs_Ps 
+        = PBS->OpHitToSimSDPs_Ps(ophit);
+      if(SDPs_Ps.empty()){
+      }else{
+        for( auto const& sdp_P : SDPs_Ps){
+          int trackId = sdp_P->trackID;
+          double split = sdp_P->numPhotons;
+          this->FillJHRV(trackId, ophit, hitCounter, split);
+        }//end for sdp_P
+      }//end if SDPs_Ps not empty
+    } //Add ophits to hitmap. 
+    //The normalization could be broken out into it's own function easily.
+    int partTracker=0;
+    for ( auto jhr : jhrV){
+      ++partTracker;
+
+      double total=0.0;
+      for(auto& htuple : jhr->hitsRec){
+        total+=std::get<2>(htuple);
+      }
+      for(auto& htuple : jhr->hitsRec){
+        //htuple->second/=total; //why bother fixing this in the entry. I can just fix it as it goes into the Tree. (No assignment call needed for the record.
+        auto const& hit = std::get<0>(htuple);
+        private_hitBuffer.update(hit->Integral();, hit->PeakTime(), hit->Width(), (std::get<2>(htuple)/total), hit->Wire(), std::get<1>(htuple), partTracker);
+        private_CalibrationCharge->Fill();
+      }
+      total=0.0;
+      for(auto& htuple : jhr->ohitsRec){
+        total+=std::get<2>(htuple);
+      }
+      for(auto& htuple : jhr->ohitsRec){
+        auto const& hit = std::get<0>(htuple);
+        private_lightBuffer.update(hit->PE();, hit->PeakTime(), hit->Width(), hit->OpChannel(), (std::get<2>(htuple)/total), hit->Wire(), std::get<1>(htuple), partTracker);
+        private_CalibrationLight->Fill();
+      }
+      //Add this entry to the TTree
+    }
+
+  } //end analyse 
+
+  //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  virtual void endJob(){
+  }
+
+}
