4cbcc63e41ce8009e0ba2ce378f4fd8b2bced48f
David Adams <dladams@bnl.gov>
2018-04-24 14:34:46 -0400

    Add AdcChannelMetric. Add string to DataMap.

---
+++ dune/DataPrep/Tool/AdcChannelMetric.h
@@ -0,0 +1,96 @@
+// AdcChannelMetric.h
+
+// David Adams
+// April 2018
+//
+// Tool to evalute metrics for single ADC channel and make histograms
+// for multiple channels. Subclasses may be used to extend the list of
+// metrics (names and algorithms).
+//
+// Configuration:
+//   LogLevel - 0=silent, 1=init, 2=each event, >2=more
+//   Metric - Name of the metric to plot:
+//              pedestal 
+//              pedestalRms
+//   FirstChannel - First channel to display
+//   LastChannel - Last+1 channel to display
+//   MetricMin - Minimum for the metric axis.
+//   MetricMax - Maximum for the metric axis.
+//   ChannelLineModulus - Repeat spacing for horizontal lines
+//   ChannelLinePattern - Pattern for horizontal lines
+//   HistName - Histogram name (should be unique within Root file)
+//   HistTitle - Histogram title
+//   PlotFileName - Name for output plot file.
+//                  If blank, no file is written.
+//                  Existing file with the same name is replaced.
+//   RootFileName - Name for the output root file.
+//                  If blank, histograms are not written out.
+//                  Existing file with the same is updated.
+// For the title and file names, the following sustitutions are made:
+//     %RUN%    --> run number
+//     %SUBRUN% --> subrun number
+//     %EVENT%  --> event number
+//     %CHAN1%  --> First channel number 
+//     %CHAN2%  --> Last channel number 
+// Drawings may include vertical lines intended to show boundaries of APAs,
+// FEMBs, wire planes, etc.
+//
+// Lines are draw at N*ChannelLineModulus + ChannelLinePattern[i] for any
+// integer N and any value if i in range of the array which are within
+// the drawn channel range.
+// If ChannelLineModulus is zero, then lines are drawn for the channels in
+// ChannelLinePattern.
+
+#ifndef AdcChannelMetric_H
+#define AdcChannelMetric_H
+
+#include "art/Utilities/ToolMacros.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "dune/DuneInterface/Tool/AdcChannelTool.h"
+#include <vector>
+
+class AdcChannelMetric : AdcChannelTool {
+
+public:
+
+  using Name = std::string;
+  using Index = unsigned int;
+  using IndexVector = std::vector<Index>;
+
+  AdcChannelMetric(fhicl::ParameterSet const& ps);
+
+  ~AdcChannelMetric() override =default;
+
+  // Tool interface.
+  DataMap view(const AdcChannelData& acd) const override;
+  DataMap viewMap(const AdcChannelDataMap& acds) const override;
+  bool updateWithView() const override { return true; }
+
+  // Local method that directly returns the metric value and units.
+  // Subclasse may overrride this to add metrics. They are expected to
+  // call the fcl ctor of this class.
+  virtual int
+  getMetric(const AdcChannelData& acd, float& metricValue, Name& metricUnits) const;
+
+private:
+
+  // Configuration data.
+  int            m_LogLevel;
+  Name           m_Metric;
+  Index          m_FirstChannel;
+  Index          m_LastChannel;
+  float          m_MetricMin;
+  float          m_MetricMax;
+  Index          m_ChannelLineModulus;
+  IndexVector    m_ChannelLinePattern;
+  int            m_Palette;
+  Name           m_HistName;
+  Name           m_HistTitle;
+  Name           m_PlotFileName;
+  Name           m_RootFileName;
+
+};
+
+DEFINE_ART_CLASS_TOOL(AdcChannelMetric)
+
+#endif
---
+++ dune/DataPrep/Tool/AdcChannelMetric_tool.cc
@@ -0,0 +1,212 @@
+// AdcChannelMetric_tool.cc
+
+#include "AdcChannelMetric.h"
+#include <iostream>
+#include <sstream>
+#include "dune/DuneCommon/TPadManipulator.h"
+#include "dune/DuneCommon/StringManipulator.h"
+#include "TH1F.h"
+#include "TCanvas.h"
+#include "TColor.h"
+#include "TStyle.h"
+#include "TDirectory.h"
+#include "TFile.h"
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+//**********************************************************************
+// Class methods.
+//**********************************************************************
+
+AdcChannelMetric::AdcChannelMetric(fhicl::ParameterSet const& ps)
+: m_LogLevel(ps.get<int>("LogLevel")) , 
+  m_Metric(ps.get<Name>("Metric")),
+  m_FirstChannel(ps.get<unsigned int>("FirstChannel")),
+  m_LastChannel(ps.get<unsigned int>("LastChannel")),
+  m_MetricMin(ps.get<float>("MetricMin")),
+  m_MetricMax(ps.get<float>("MetricMax")),
+  m_ChannelLineModulus(ps.get<Index>("ChannelLineModulus")),
+  m_ChannelLinePattern(ps.get<IndexVector>("ChannelLinePattern")),
+  m_HistName(ps.get<Name>("HistName")),
+  m_HistTitle(ps.get<Name>("HistTitle")),
+  m_PlotFileName(ps.get<Name>("PlotFileName")),
+  m_RootFileName(ps.get<Name>("RootFileName"))
+  {
+
+  const string myname = "AdcChannelMetric::ctor: ";
+  if ( m_LogLevel ) {
+    cout << myname << "Configuration: " << endl;
+    cout << myname << "            LogLevel: " << m_LogLevel << endl;
+    cout << myname << "              Metric: " << m_Metric << endl;
+    cout << myname << "        FirstChannel: " << m_FirstChannel << endl;
+    cout << myname << "         LastChannel: " << m_LastChannel << endl;
+    cout << myname << "           MetricMin: " << m_MetricMin << endl;
+    cout << myname << "           MetricMax: " << m_MetricMax << endl;
+    cout << myname << "  ChannelLineModulus: " << m_ChannelLineModulus << endl;
+    cout << myname << "  ChannelLinePattern: {";
+    bool first = true;
+    for ( Index icha : m_ChannelLinePattern ) {
+      if ( ! first ) cout << ", ";
+      first = false;
+      cout << icha;
+    }
+    cout << "}" << endl;
+    cout << myname << "            HistName: " << m_HistName << endl;
+    cout << myname << "           HistTitle: " << m_HistTitle << endl;
+    cout << myname << "        PlotFileName: " << m_PlotFileName << endl;
+    cout << myname << "        RootFileName: " << m_RootFileName << endl;
+  }
+}
+
+//**********************************************************************
+
+DataMap AdcChannelMetric::view(const AdcChannelData& acd) const {
+  const string myname = "AdcChannelMetric::view: ";
+  DataMap ret;
+  float val = 0.0;
+  Name sunits;
+  int rstat = getMetric(acd, val, sunits);
+  if ( rstat ) return ret.setStatus(rstat);
+  ret.setString("metricName", m_Metric);
+  ret.setFloat("metricValue", val);
+  ret.setString("metricUnits", sunits);
+  return ret;
+}
+
+//**********************************************************************
+
+DataMap AdcChannelMetric::viewMap(const AdcChannelDataMap& acds) const {
+  const string myname = "AdcChannelMetric::viewMap: ";
+  DataMap ret;
+  if ( acds.size() == 0 ) {
+    cout << myname << "Input channel map is empty." << endl;
+    return ret.setStatus(1);
+  }
+  const AdcChannelData& acdFirst = acds.begin()->second;
+  const AdcChannelData& acdLast = acds.rbegin()->second;
+  AdcIndex chanFirst = m_FirstChannel;
+  AdcIndex chanLast = m_LastChannel;
+  if ( chanLast <= chanFirst ) {
+    chanFirst = acdFirst.channel;
+    chanLast = acdLast.channel;
+  }
+  if ( m_LogLevel >= 2 ) cout << "Processing " << acds.size() << " channels in the range ("
+                              << chanFirst << ", " << chanLast << ")" << endl;
+  // Create title and file names.
+  string hname = m_HistName;
+  string htitl = m_HistTitle;
+  string ofname = m_PlotFileName;
+  string ofrname = m_RootFileName;
+  vector<string*> strs = {&hname, &htitl, &ofname, &ofrname};
+  for ( string* pstr : strs ) {
+    string& str = *pstr;
+    StringManipulator sman(str);
+    if ( acdFirst.run != AdcChannelData::badIndex ) sman.replace("%RUN%", acdFirst.run);
+    else sman.replace("%RUN%", "RunNotFound");
+    if ( acdFirst.subRun != AdcChannelData::badIndex ) sman.replace("%SUBRUN%", acdFirst.subRun);
+    else sman.replace("%SUBRUN%", "SubRunNotFound");
+    if ( acdFirst.event != AdcChannelData::badIndex ) sman.replace("%EVENT%", acdFirst.event);
+    else sman.replace("%EVENT%", "EventNotFound");
+    sman.replace("%CHAN1%", chanFirst);
+    sman.replace("%CHAN2%", chanLast);
+  }
+  Index nchan = chanLast - chanFirst + 1;
+  string sttl;
+  string slaby;
+  if ( m_Metric == "pedestal" ) {
+    sttl = "ADC pedestal";
+    slaby = "Pedestal";
+  } else if ( m_Metric == "pedestalRms" ) {
+    sttl = "ADC pedestal RMS";
+    slaby = "Pedestal RMS";
+  } else {
+    cout << myname << "ERROR: Invalid metric name: " << m_Metric << endl;
+    return ret.setStatus(1);
+  }
+  if ( htitl == "AUTO" ) htitl = sttl;
+  TH1* ph = new TH1F(hname.c_str(), htitl.c_str(), nchan, chanFirst, chanLast+1);
+  ph->SetDirectory(nullptr);
+  ph->SetLineWidth(2);
+  ph->SetStats(0);
+  Index nfill = 0;
+  AdcChannelDataMap::const_iterator iacd1=acds.lower_bound(chanFirst);
+  AdcChannelDataMap::const_iterator iacd2=acds.upper_bound(chanLast);
+  float val = 0.0;
+  Name sunits;
+  for ( AdcChannelDataMap::const_iterator iacd=iacd1; iacd!=iacd2; ++iacd ) {
+    const AdcChannelData& acd = iacd->second;
+    int rstat = getMetric(acd, val, sunits);
+    if ( rstat ) {
+      cout << myname << "WARNING: Metric evaluation failed for channel " << acd.channel << endl;
+      continue;
+    }
+    Index icha = iacd->first;
+    Index bin = icha - chanFirst + 1;
+    ph->SetBinContent(bin, val);
+    if ( nfill == 0 ) {
+      if ( sunits.size() ) {
+        slaby = slaby + " [" + sunits + "]";
+      }
+      ph->GetXaxis()->SetTitle("Channel");
+      ph->GetYaxis()->SetTitle(slaby.c_str());
+    }
+    ++nfill;
+  }
+  if ( m_LogLevel >= 3 ) cout << myname << "Filled " << nfill << " channels." << endl;
+  if ( ofname.size() ) {
+    TPadManipulator man;
+    man.add(ph, "hist");
+    man.addAxis();
+    if ( m_ChannelLineModulus ) {
+      for ( Index icha : m_ChannelLinePattern ) {
+        man.addVerticalModLines(m_ChannelLineModulus, icha);
+      }
+    } else {
+      for ( Index icha : m_ChannelLinePattern ) {
+        if ( icha > chanFirst && icha < chanLast ) {
+          man.addVerticalLine(icha, 1.0, 3);
+        }
+      }
+    }
+    if ( m_MetricMax > m_MetricMin ) man.setRangeY(m_MetricMin, m_MetricMax);
+    man.print(ofname);
+  }
+  if ( m_LogLevel > 1 ) {
+    cout << myname << "Created plot ";
+    cout << "for " << nfill << " channels in range " << chanFirst << " - " << chanLast << endl;
+    cout << myname << " Output file: " << ofname << endl;
+  }
+  if ( ofrname.size() ) {
+    TFile* pfile = TFile::Open(ofrname.c_str(), "UPDATE");
+    ph->Write();
+    if ( m_LogLevel > 1 ) cout << myname << "Wrote " << ph->GetName() << " to " << ofrname << endl;
+    delete pfile;
+  }
+  ret.setHist(ph, true);
+  return ret;
+}
+
+//**********************************************************************
+
+int AdcChannelMetric::getMetric(const AdcChannelData& acd, float& val, Name& sunits) const {
+  const string myname = "AdcChannelMetric::getMetric: ";
+  val = 0.0;
+  sunits = "";
+  if ( m_Metric == "pedestal" ) {
+    val = acd.pedestal;
+    sunits = "ADC counts";
+  } else if ( m_Metric == "pedestalRms" ) {
+    val = acd.pedestalRms;
+    sunits = "ADC counts";
+  } else {
+    cout << myname << "ERROR: Invalid metric name: " << m_Metric << endl;
+    return 1;
+  }
+  return 0;
+}
+
+//**********************************************************************
