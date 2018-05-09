5e0559722c15099153fe8fcc05cab9e07b0c2617
Jason Stock <jstock@dunegpvm05.fnal.gov>
2018-05-09 17:36:30 -0500

    Adding a new folder to serve as an alanog of lardataobj for dunespecific objects.

---
+++ dune/DuneObjBase/classes.h
@@ -0,0 +1,17 @@
+// Build a dictionary.
+//
+// $Author:  $
+// $Date: 2010/04/12 18:12:28 $
+//
+// Original author Rob Kutschke, modified by klg
+
+#include "canvas/Persistency/Common/PtrVector.h"
+#include "canvas/Persistency/Common/Wrapper.h"
+#include "canvas/Persistency/Common/Assns.h"
+
+#include "dune/DuneObjBase/EventRecord.h"
+#include "lardataobj/RecoBase/OpFlash.h"
+#include "lardataobj/RecoBase/Hit.h"
+
+namespace {
+}
