2fd1fa09203ec25e51f41cb5244269362aef4a6a
Tingjun Yang <tjyang@fnal.gov>
2018-06-02 15:44:44 -0500

    temp commit.

--- dune/HitAnalysis/SignalToNoise_module.cc
+++ dune/HitAnalysis/SignalToNoise_module.cc
@@ -174,6 +174,19 @@ void dune::SignalToNoise::analyze(art::Event const & e)
       tracklist[i]->Direction(larStart,larEnd);
       double dc = std::abs(ccosx*larStart[0]+ccosy*larStart[1]+ccosz*larStart[2]);
       dcos->Fill(dc);
+      if (i==1){
+        for (size_t j = 0; j<tracklist[i]->NPoints(); ++j){
+          if (tracklist[i]->HasValidPoint(j)){
+            std::cout<<j<<" "<<tracklist[i]->LocationAtPoint(j).X()
+                     <<" "<<tracklist[i]->LocationAtPoint(j).Y()
+                     <<" "<<tracklist[i]->LocationAtPoint(j).Z()
+                     <<" "<<tracklist[i]->DirectionAtPoint(j).X()
+                     <<" "<<tracklist[i]->DirectionAtPoint(j).Y()
+                     <<" "<<tracklist[i]->DirectionAtPoint(j).Z()
+                     <<std::endl;
+          }
+        }
+      }
       if (dc>0.98){//found a match
         double ctime = (countlist[ci1[0]]->GetTrigTime() + countlist[ci2[0]]->GetTrigTime())/(2*32.); //ticks
         double x0 = trackStart.X() + (trackStart.X()>0?-1:1)*ctime*0.5*0.1085;
