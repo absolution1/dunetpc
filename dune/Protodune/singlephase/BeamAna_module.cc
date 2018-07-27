633a80a7f7b5dc2f6d35072516cb8cd69e9ffa3a
Jacob Calcutt <calcuttj@msu.edu>
2018-07-27 16:58:20 -0500

    Added in configurable device list. Cleaned up module

--- dune/Protodune/singlephase/BeamAna_module.cc
+++ dune/Protodune/singlephase/BeamAna_module.cc
@@ -64,6 +64,8 @@ private:
   std::string fURLStr;
   uint64_t fFixedTime;
   std::vector<uint64_t> fMultipleTimes;
+  std::vector<std::string> fDevices;
+  std::string fPrefix;
 };
 
 
@@ -81,9 +83,6 @@ void proto::BeamAna::analyze(art::Event const & e)
   std::cerr << "%%%%%%%%%% Getting ifbeam service handle %%%%%%%%%%" << std::endl;
   art::ServiceHandle<ifbeam_ns::IFBeam> ifb;
   std::cerr << "%%%%%%%%%% Got ifbeam handle %%%%%%%%%%" << std::endl;
-
-  std::cout << "TESTING BINARY" << std::endl;
-  std::cout << toBinary(long(5.)) << std::endl;
 
   std::unique_ptr<ifbeam_ns::BeamFolder> bfp = ifb->getBeamFolder(fBundleName,fURLStr,fTimeWindow);
   std::cerr << "%%%%%%%%%% Got beam folder %%%%%%%%%%" << std::endl;
@@ -105,14 +104,13 @@ void proto::BeamAna::analyze(art::Event const & e)
     std::cout <<" Using Event Time: " << uint64_t(e.time().timeLow()) << std::endl;
     fMultipleTimes.push_back(uint64_t(e.time().timeLow()));
   }
-  std::string prefix = "dip/acc/NORTH/NP04/BI/XBPF/";
-  std::string devices[6] = {"XBPF022697_V",
-                            "XBPF022697_H",
-                            "XBPF022707_V",
-                            "XBPF022707_H",
-                            "XBPF022716_V",
-                            "XBPF022716_H"};
-  size_t nDev = 6;
+
+  size_t nDev = fDevices.size();
+  std::cout << "Using devices: " << fPrefix << std::endl;
+  for(size_t id = 0; id < fDevices.size(); ++id){
+    std::cout << fPrefix + fDevices[id] << std::endl;
+  }
+
   beamspill::ProtoDUNEBeamSpill * spill = new beamspill::ProtoDUNEBeamSpill();
   spill->InitFBMs(nDev);
 
@@ -120,9 +118,9 @@ void proto::BeamAna::analyze(art::Event const & e)
   for(size_t it = 0; it < fMultipleTimes.size(); ++it){
     std::cout << "Time: " << fMultipleTimes[it] << std::endl;
     for(int d = 0; d < 6; ++d){
-      std::cout <<"Device: " << devices[d] << std::endl;
-      std::vector<double> data = bfp->GetNamedVector(fMultipleTimes[it], prefix + devices[d] + ":eventsData[]");
-      std::vector<double> counts = bfp->GetNamedVector(fMultipleTimes[it], prefix + devices[d] + ":countsRecords[]");
+      std::cout <<"Device: " << fDevices[d] << std::endl;
+      std::vector<double> data = bfp->GetNamedVector(fMultipleTimes[it], fPrefix + fDevices[d] + ":eventsData[]");
+      std::vector<double> counts = bfp->GetNamedVector(fMultipleTimes[it], fPrefix + fDevices[d] + ":countsRecords[]");
     
   
       std::cout << "Data: " << data.size() << std::endl;
@@ -134,12 +132,12 @@ void proto::BeamAna::analyze(art::Event const & e)
 
       beamspill::FBM fbm;
       fbm.ID = d;
-
+           
       for(size_t i = 0; i < counts[1]; ++i){
         std::cout << "Count: " << i << std::endl;
         for(int j = 0; j < 10; ++j){
           double theData = data[20*i + (2*j + 1)];
-          std::cout << std::setw(12) << theData ;
+          std::cout << std::setw(15) << theData ;
           if(j < 4){
             fbm.timeData[j] = theData;           
           }
@@ -155,22 +153,23 @@ void proto::BeamAna::analyze(art::Event const & e)
         spill->DecodeFibers(d,i);
       }
 
-      for(size_t i = 0; i < counts[1]; ++i){
-        std::cout << "Count: " << i << std::endl;
-        for(int j = 0; j < 10; ++j){
-          double theData = data[20*i + (2*j + 1)];
-          toBinary(long(theData));        
-
-        }
-        std::cout <<std::endl;
-      }
     }
   }
 
   for(size_t id = 0; id < 6; ++id){
-    std::cout << "FBM: " << id << std::endl;
+    std::cout << "FBM: " << fDevices[id] << std::endl;
     std::cout << "N Triggers: " << spill->GetNFBMTriggers(id) << std::endl;
-
+    
+    for(size_t it = 0; it < spill->GetNFBMTriggers(id); ++it){
+      std::cout << "Trigger: " << it <<std::endl;
+      std::cout << "Hit Fibers: ";
+      for(size_t iF = 0; iF < 192; ++iF){
+        if( spill->GetFiberStatus(id,it,iF) ){
+          std::cout << iF << " ";
+        }
+      }
+      std::cout << std::endl;
+    }
   }
 
 
@@ -214,18 +213,12 @@ void proto::BeamAna::reconfigure(fhicl::ParameterSet const & p)
   fTimeWindow  = p.get<double>("TimeWindow");
   fFixedTime   = p.get<uint64_t>("FixedTime");
   fMultipleTimes = p.get< std::vector<uint64_t> >("MultipleTimes");
+  fDevices     = p.get< std::vector<std::string> >("Devices");
+  fPrefix      = p.get<std::string>("Prefix");
 }
 
 std::bitset<sizeof(long)*CHAR_BIT> proto::BeamAna::toBinary(long num){
-/*  std::cout << "double Size: " << sizeof(double)*CHAR_BIT << std::endl;
-  std::cout << "long size: " << sizeof(unsigned long)*CHAR_BIT << std::endl;*/
-
-//  /*unsigned*/ long * p = reinterpret_cast</*unsigned*/ long*>(&num);
-/*  std::cout << "P! " << p << std::endl;
-  std::cout << "bits: " << std::bitset<32>(p[0]) << " " << std::bitset<32>(p[1]) << std::endl;
-  std::cout << "MyBits: " << std::bitset<64>(*p) << std::endl;*/
-//  std::bitset<sizeof(double)*CHAR_BIT> mybits(*p);
-    
+   
   std::bitset<sizeof(double)*CHAR_BIT> mybits(num);  
   std::bitset<32> upper, lower;
   for(int i = 0; i < 32; ++i){
@@ -233,10 +226,7 @@ std::bitset<sizeof(long)*CHAR_BIT> proto::BeamAna::toBinary(long num){
     upper[i] = mybits[i + 32];   
   }
   if(upper.any()) std::cout << "WARNING: NONZERO HALF" << std::endl;
-  /*char * p = reinterpret_cast<char*>(&num);
-  for (int i = sizeof(double)*CHAR_BIT-1 ; i >= 0 ; --i){
-    mybits.set(i, (*(p)&(1<<i) ));
-  }*/
+
   return mybits;
 }
 
