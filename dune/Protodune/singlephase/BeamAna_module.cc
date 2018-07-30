cb38d08eecc036d5947c447318885c36a6bcbb28
Jacob Calcutt <calcuttj@msu.edu>
2018-07-30 17:03:40 -0500

    Started adding some configurability for FBM placement and orientation. Need to tidy up and finalize that later. Added method to save TPolyLine3Ds of tracks in the beamline.

--- dune/Protodune/singlephase/BeamAna_module.cc
+++ dune/Protodune/singlephase/BeamAna_module.cc
@@ -18,10 +18,14 @@
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 #include "IFBeam_service.h"
+#include "art/Framework/Services/Optional/TFileService.h"
 //#include "dune/BeamData/ProtoDUNEBeamSpill/ProtoDUNEBeamSpill.h"
 #include "dune/DuneObj/ProtoDUNEBeamSpill.h"
 #include <bitset>
 #include <iomanip>
+
+#include "TTree.h"
+#include "TPolyLine3D.h"
 
 namespace proto {
   class BeamAna;
@@ -51,10 +55,14 @@ public:
   void endRun(art::Run const & r) override;
   void endSubRun(art::SubRun const & sr) override;
   void reconfigure(fhicl::ParameterSet const & p);
-  std::bitset<sizeof(double)*CHAR_BIT> toBinary(const long num);
-  void toBits(double);
+  std::bitset<sizeof(double)*CHAR_BIT> toBinary(const long num);  
+  void matchTriggers(beamspill::ProtoDUNEBeamSpill spill);
+  double GetPosition(size_t, size_t);
 
 private:
+  
+  TTree * fOutTree;
+  TPolyLine3D * theLine;
 
   // Declare member data here.
   //bool fLoadFromDB; // unused
@@ -63,8 +71,11 @@ private:
   std::string fBundleName;
   std::string fURLStr;
   uint64_t fFixedTime;
-  std::vector<uint64_t> fMultipleTimes;
-  std::vector<std::string> fDevices;
+  std::vector< uint64_t > fMultipleTimes;
+  std::vector< std::string > fDevices;
+  std::vector< std::array<double, 3> > fCoordinates; 
+  std::vector< std::array<double, 3> > fRotations;
+  std::vector< double > fFiberDimension;
   std::string fPrefix;
 };
 
@@ -109,8 +120,15 @@ void proto::BeamAna::analyze(art::Event const & e)
   std::cout << "Using devices: " << fPrefix << std::endl;
   for(size_t id = 0; id < fDevices.size(); ++id){
     std::cout << fPrefix + fDevices[id] << std::endl;
+    std::cout << "At: " << fCoordinates[id][0] << " " << fCoordinates[id][1] << " " << fCoordinates[id][2] << std::endl;
+    std::cout << "Rotated: " << fRotations[id][0] << " " << fRotations[id][1] << " " << fRotations[id][2] << std::endl;
   }
 
+  
+ 
+  
+
+  //Start getting spill information
   beamspill::ProtoDUNEBeamSpill * spill = new beamspill::ProtoDUNEBeamSpill();
   spill->InitFBMs(nDev);
 
@@ -134,10 +152,10 @@ void proto::BeamAna::analyze(art::Event const & e)
       fbm.ID = d;
            
       for(size_t i = 0; i < counts[1]; ++i){
-        std::cout << "Count: " << i << std::endl;
+//        std::cout << "Count: " << i << std::endl;
         for(int j = 0; j < 10; ++j){
           double theData = data[20*i + (2*j + 1)];
-          std::cout << std::setw(15) << theData ;
+//          std::cout << std::setw(15) << theData ;
           if(j < 4){
             fbm.timeData[j] = theData;           
           }
@@ -146,7 +164,7 @@ void proto::BeamAna::analyze(art::Event const & e)
           }
         }
         spill->AddFBMTrigger(d,fbm);
-        std::cout << std::endl;
+//        std::cout << std::endl;
       }
 
       for(size_t i = 0; i < spill->GetNFBMTriggers(d); ++i){
@@ -156,7 +174,7 @@ void proto::BeamAna::analyze(art::Event const & e)
     }
   }
 
-  for(size_t id = 0; id < 6; ++id){
+/*  for(size_t id = 0; id < 6; ++id){
     std::cout << "FBM: " << fDevices[id] << std::endl;
     std::cout << "N Triggers: " << spill->GetNFBMTriggers(id) << std::endl;
     
@@ -171,12 +189,58 @@ void proto::BeamAna::analyze(art::Event const & e)
       std::cout << std::endl;
     }
   }
+*/
 
+ //Setting some dummy Triggers for drawing  
+
+ double dummyTriggerTimeLSB[6] = {1.50000e+08,1.50000e+08,1.50002e+08,1.50002e+08,1.50004e+08,1.50004e+08};
+ double dummyTriggerTimeMSB[6] = {1.53191e+09,1.53191e+09,1.53191e+09,1.53191e+09,1.53191e+09,1.53191e+09};
+ double dummyEventTimeLSB[6]   = {1.50000e+08,1.50000e+08,1.50000e+08,1.50000e+08,1.50000e+08,1.50000e+08};
+ double dummyEventTimeMSB[6]   = {1.53191e+09,1.53191e+09,1.53191e+09,1.53191e+09,1.53191e+09,1.53191e+09};
+
+ double dummyHit[6][6]     = { {1,0,0,0,0,0},
+                               {1,0,0,0,0,0},
+                               {0,0,1,0,0,0},
+                               {0,0,1,0,0,0},
+                               {0,0,0,0,1,0},
+                               {0,0,0,0,1,0}}; //first index: device, second LSB->MSB
+
+ for(size_t id = 0; id < 6; ++id){
+   beamspill::FBM fbm;
+   fbm.ID = id;
+
+   fbm.timeData[0] = dummyTriggerTimeLSB[id];
+   fbm.timeData[1] = dummyTriggerTimeMSB[id];
+   fbm.timeData[2] = dummyEventTimeLSB[id];
+   fbm.timeData[3] = dummyEventTimeMSB[id];
+
+   for(int i = 0; i < 6; ++i){
+     fbm.fiberData[i] = dummyHit[id][i];
+   }
+
+   spill->AddFBMTrigger(id,fbm);
+   spill->DecodeFibers(id,spill->GetNFBMTriggers(id) - 1);//Decode the last one
+   for(size_t iF = 0; iF < 192; ++iF){
+     if( spill->GetFiberStatus(id,spill->GetNFBMTriggers(id) - 1,iF) ){
+       std::cout << iF << " ";
+     }
+   }
+   std::cout << std::endl;
+ }
+
+ matchTriggers(*spill);
 
 }
 
 void proto::BeamAna::beginJob()
 {
+
+  art::ServiceHandle<art::TFileService> tfs;
+
+  fOutTree = tfs->make<TTree>("tree", "lines"); 
+  //Need to make this configurable later
+  theLine = new TPolyLine3D(3);
+  fOutTree->Branch("Line", &theLine);
   // Implementation of optional member function here.
 }
 
@@ -213,7 +277,18 @@ void proto::BeamAna::reconfigure(fhicl::ParameterSet const & p)
   fTimeWindow  = p.get<double>("TimeWindow");
   fFixedTime   = p.get<uint64_t>("FixedTime");
   fMultipleTimes = p.get< std::vector<uint64_t> >("MultipleTimes");
-  fDevices     = p.get< std::vector<std::string> >("Devices");
+  fDevices     = p.get< std::vector< std::string > >("Devices");
+//  fDeviceType  = p.get< std::vector<std::string> >("DeviceTypes");
+
+  //Location of Device 
+  fCoordinates = p.get< std::vector< std::array<double,3> > >("Coordinates");
+  
+  //Rotation of Device
+  fRotations   = p.get< std::vector< std::array<double,3> > >("Rotations"); 
+
+  //Deminsion of Fibers
+  fFiberDimension = p.get< std::vector<double> >("Dimension");
+
   fPrefix      = p.get<std::string>("Prefix");
 }
 
@@ -230,36 +305,86 @@ std::bitset<sizeof(long)*CHAR_BIT> proto::BeamAna::toBinary(long num){
   return mybits;
 }
 
-void proto::BeamAna::toBits(double input){
+void proto::BeamAna::matchTriggers(beamspill::ProtoDUNEBeamSpill spill){
+  
+  std::vector<size_t> goodTriggers;
+  bool foundNext = false;
 
-  std::cout << "size: " << sizeof(double) << std::endl;
-  unsigned char rawBytes[sizeof(double)];  
-  memcpy(rawBytes,&input,sizeof(double));
+  //Start with the earliest device. Should be most upstream
+  //Go through each trigger and try to match
+  for(size_t it = 0; it < spill.GetNFBMTriggers(0); ++it){
 
-  unsigned char startMask=1;
-  while (0!=static_cast<unsigned char>(startMask<<1)) {
-    startMask<<=1;
-  }
+    double time = spill.DecodeFiberTime(0, it);
+    std::cout << time << std::endl;
 
-  bool hasLeadBit=false;
+    //Go through the next FBMs and see if they have a good time
+    for(size_t id = 1; id < fDevices.size(); ++id){
+      foundNext = false;
 
-  size_t byteIndex;
-  for (byteIndex=0; byteIndex<sizeof(double); ++byteIndex){
-    unsigned char bitMask=startMask;
-    while (0!=bitMask) {
-      if (0!=(bitMask&rawBytes[byteIndex])) {
-        std::cout<<"1";
-        hasLeadBit=true;
-      } 
-      else if (hasLeadBit) {
-        std::cout<<"0";
+      for (size_t it2 = 0; it2 < spill.GetNFBMTriggers(id); ++it2){
+        std::cout << (spill.DecodeFiberTime(id, it2 ) - time) << std::endl;
+
+        if ( ( (spill.DecodeFiberTime(id, it2 ) - time) < 0.00010e+08 ) && ( (spill.DecodeFiberTime(id, it2 ) - time) >= 0 ) ){
+
+          std::cout << "Found matching trigger in " << id << " " << spill.DecodeFiberTime(id, it2) << std::endl; 
+          foundNext = true;
+          if(goodTriggers.empty()) goodTriggers.push_back(it);
+          goodTriggers.push_back(it2);
+
+          //For now: break after first one
+          break;
+        }       
       }
-      bitMask>>=1;
+      //Break after missing a device in the sequence
+      if(!foundNext){
+        goodTriggers.clear();
+        break;
+      }      
+    }
+    //Using found next from last one
+    if(foundNext){
+      std::cout << "Found good track" << std::endl;
+        
+      if(theLine->GetN()){
+        //Reset it
+        *theLine = TPolyLine3D();
+      }
+
+      std::vector<double> xPos;      
+      std::vector<double> yPos;      
+      for(size_t ig = 0; ig < goodTriggers.size(); ++ig){
+        std::vector<short> active = spill.GetActiveFibers(ig, goodTriggers[ig]);
+        std::cout << "Trigger " << ig << " has " << active.size() << " active fibers "<< active[0] << std::endl;        
+        //just get the first
+        double position = GetPosition(ig, active[0]);                
+        if(!(ig % 2)){ //Even numbers
+          xPos.push_back(position);
+        }
+        else{ //Odd numbers
+          yPos.push_back(position);         
+        }
+      }
+      std::cout << "Sizes: " << xPos.size() << " " << yPos.size() << std::endl;
+      //Check if diff size?
+      for(size_t ip = 0; ip < xPos.size(); ++ip){
+        theLine->SetPoint(ip, xPos.at(ip), yPos.at(ip), fCoordinates[2*ip][2]);
+      }
+
+      fOutTree->Fill();
     }
   }
-  if(!hasLeadBit){
-    std::cout << "0";
-  }
+
+  
+}
+
+double proto::BeamAna::GetPosition(size_t iDevice, size_t iFiber){
+  if(iFiber > 192){ std::cout << "Please select fiber in range [0,191]" << std::endl; return -1.;}
+  if(iDevice > fDevices.size() -1 ){ std::cout << "Device out of range " << std::endl; return -1.;}
+  double size = fFiberDimension[iDevice];
+  
+  //Define 0th fiber as origin. Go to middle of the fiber
+  double pos = size*iFiber + size/2.;
+  return pos;
 }
 
 DEFINE_ART_MODULE(proto::BeamAna)
