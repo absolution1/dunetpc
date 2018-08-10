038396dc1bff510f25cc94205d45c5ef657cf82a
David Adams <dladams@bnl.gov>
2018-08-10 18:30:58 -0400

    Add ProtoduneOnlineChannel to map to online.

--- dune/Protodune/singlephase/Tool/PdspOnlineChannel.h
+++ dune/Protodune/singlephase/Tool/PdspOnlineChannel.h
@@ -5,12 +5,16 @@
 //
 // Tool that converts a protodune SP offline channel number
 // to an online number with the convention that
-//   chanOn = 2560*(APA-1) + 512*WIB_in_APA + 128*FEMB_in_WIB + CHAN_in_FEMB
+//   chanOn = 2560*(APA) + 512*WIB_in_APA + 128*FEMB_in_WIB + CHAN_in_FEMB
 //
 // From this, the APA, global FEMB and FEMB channel are
 //           APA = chanOn/2560 + 1
 //          FEMB = chanOn/128
 //  CHAN_in_FEMB = chanOn % 128
+//
+// NOte that the FEMB ordering is most likely not geometric and that the
+// indices are obtained from PdspChannelMapService which has unusual
+// and varying values.
 
 #ifndef PdspOnlineChannel_H
 #define PdspOnlineChannel_H
