834732c085ed55db8a062c28b8c14cc8475349e0
David Adams <dladams@bnl.gov>
2018-08-21 14:33:57 -0400

    Allow tickmod tree file name config.

--- dune/DataPrep/Tool/AdcTickModViewer_tool.cc
+++ dune/DataPrep/Tool/AdcTickModViewer_tool.cc
@@ -583,7 +583,8 @@ int AdcTickModViewer::processAccumulation(Index& nplot) const {
   if ( m_TreeFileName.size() ) {
     if ( m_LogLevel >= 2 ) cout << myname << "Creating tickmod tree." << endl;
     TDirectory* psavdir = gDirectory;
-    pfile = TFile::Open(m_TreeFileName.c_str(), "RECREATE");
+    string tfname = nameReplace (m_TreeFileName, state().currentAcd, 0);
+    pfile = TFile::Open(tfname.c_str(), "RECREATE");
     if ( pfile->IsOpen() ) {
       ptree = new TTree("tickmod", "TickMod tree");
       ptree->Branch("data", &(state().treedata), 64000, 1);
