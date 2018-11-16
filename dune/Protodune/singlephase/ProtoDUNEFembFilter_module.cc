e4f3082dab8fb6aa6b6fb39d37d8fce1f046171f
Owen Goodwin <ogoodwin@dunegpvm04.fnal.gov>
2018-11-16 17:32:54 -0600

    added option select all fembs or beam side only fembs

--- dune/Protodune/singlephase/ProtoDUNEFembFilter_module.cc
+++ dune/Protodune/singlephase/ProtoDUNEFembFilter_module.cc
@@ -33,6 +33,7 @@ namespace filt{
   private:
 
     unsigned int fLogLevel;
+    bool fRequireBeamsideFembsOnly;
     
     TH1D* fSelectedEvents;
     TH1D* fTotalEvents;
@@ -42,9 +43,16 @@ namespace filt{
   ProtoDUNEFembFilter::ProtoDUNEFembFilter::ProtoDUNEFembFilter(fhicl::ParameterSet const & pset) {
 
     fLogLevel = pset.get<unsigned int>("LogLevel");
-
+    fRequireBeamsideFembsOnly = pset.get<bool>("RequireBeamsideFembsOnly");
     if ( fLogLevel >= 1 ) {
       std::cout  << "                LogLevel: " << fLogLevel << std::endl;
+      if(fRequireBeamsideFembsOnly){
+        std::cout  << "Filtering events with inactive FEMBs on beamside APAs"<< std::endl;
+      }
+    
+      else{
+        std::cout  << "Filtering events with any inactive FEMBs"<< std::endl;
+      }
 
       }
     
@@ -60,20 +68,38 @@ namespace filt{
 
   bool ProtoDUNEFembFilter::filter(art::Event & evt) {
     std::vector<int> BeamsideAPAs;
+    std::vector<int> AllAPAs;
+    std::vector<int> checkedAPAs;
+
     // Add some elements to myIntVector
     BeamsideAPAs.push_back(0);
     BeamsideAPAs.push_back(2);
     BeamsideAPAs.push_back(4);
 
+    AllAPAs.push_back(0);
+    AllAPAs.push_back(1);
+    AllAPAs.push_back(2);
+    AllAPAs.push_back(3);
+    AllAPAs.push_back(4);
+    AllAPAs.push_back(5);
+
+
     const std::string myname = "ProtoDUNEFembFilter::filter: ";
 
+    if(fRequireBeamsideFembsOnly){
+      checkedAPAs=BeamsideAPAs;
+    }
+    else{
+      checkedAPAs=AllAPAs;
+    }
+    
     bool keep = true;
     // Helper utility functions
     protoana::ProtoDUNEDataUtils dataUtil;
     //ProtoDUNEDataUtils dataUtil;
 
     fTotalEvents->Fill(1); //count total events
-    for (auto APA = BeamsideAPAs.begin(); APA != BeamsideAPAs.end(); ++APA){ //loop through beam side APAs
+    for (auto APA = checkedAPAs.begin(); APA != checkedAPAs.end(); ++APA){ //loop through beam side APAs
       //std::cout<<"APA:"<<*APA<<std::endl;
       //std::cout<<dataUtil.GetNActiveFembsForAPA(evt, *APA)<<std::endl;
       if (dataUtil.GetNActiveFembsForAPA(evt, *APA)!=20){ //check if APA has all 20 fembs active
