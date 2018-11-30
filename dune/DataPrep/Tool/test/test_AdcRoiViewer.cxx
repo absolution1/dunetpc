f08424c5bce857d078e6533e86c91fa8813a9978
David Adams <dladams@bnl.gov>
2018-11-30 14:45:22 -0500

    Add ranges to AdcRoiViewer chansum plots.

--- dune/DataPrep/Tool/test/test_AdcRoiViewer.cxx
+++ dune/DataPrep/Tool/test/test_AdcRoiViewer.cxx
@@ -78,9 +78,9 @@ int test_AdcRoiViewer(bool useExistingFcl =false) {
     fout << "]" << endl;
     fout << "tools.mytool2.ChanSumHists: [" << endl;
     fout << "  {name:\"hcsHeight_%CRNAME%\" title:\"Pulse heights for run %RUN% %CRLABEL%\" "
-         <<    "valHist:\"hfh_%0RUN%_chan%0CHAN%\" valType:fitMean errType:fitSigma cr:apa1x plot:\"\"}," << endl;
+         <<    "valHist:\"hfh_%0RUN%_chan%0CHAN%\" valType:fitMean errType:fitSigma cr:apa1x plot:\"\" pran:\"\"}," << endl;
     fout << "  {name:\"hcsWidth_%CRNAME%\" title:\"Shaping times for run %RUN% %CRLABEL%\" "
-         <<    "valHist:\"hfw_%0RUN%_chan%0CHAN%\" valType:fitMean errType:fitSigma cr:apa1x plot:\"\"}" << endl;
+         <<    "valHist:\"hfw_%0RUN%_chan%0CHAN%\" valType:fitMean errType:fitSigma cr:apa1x plot:\"\" pran:\"\"}" << endl;
     fout << "]" << endl;
     //fout << "  {val:\"hfw_%0RUN%_chan%0CHAN%:FitMean\" hist:\"hcsWidth_%0RUN%\"
     fout << "tools.mytool2.RoiRootFileName: \"\"" << endl;
