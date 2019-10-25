f55888e02e42686fc2d263415490780ed4076ff1
David Adams <dladams@bnl.gov>
2019-10-25 17:06:32 -0500

    Enhance pdChannelrange to support apa7.

--- dune/Protodune/singlephase/Exe/pdChannelRange.cxx
+++ dune/Protodune/singlephase/Exe/pdChannelRange.cxx
@@ -15,6 +15,7 @@
 
 using std::string;
 using std::cout;
+using std::cin;
 using std::endl;
 using std::ostream;
 using std::istringstream;
@@ -25,25 +26,36 @@ int main(int argc, char** argv) {
   const string myname = "pdChannelRange: ";
   bool help = argc == 1;
   string crname;
-  string fclfile = "protodune_tools_dune.fcl";
   string tnameRange = "channelRanges";
   string tnameGroup = "channelGroups";
-  if ( argc > 1 ) {
-    string arg = argv[1];
+  int iarg = 1;
+  string sdet = "protodune";
+  if ( argc > iarg ) {
+    string arg = argv[iarg];
     if ( arg == "-h" ) help = true;
-    else crname = argv[1];
+    else {
+      if ( arg == "-d" ) {
+        if ( argc > ++iarg ) sdet = argv[iarg];
+        else help = true;
+      }
+      if ( argc > ++iarg ) crname = argv[iarg];
+    }
   }
+  string fclfile = sdet + "_tools_dune.fcl";
   if ( help ) {
-    cout << "Usage: " << argv[0] << " NAME" << endl;
-    cout << "  Displays the group for channel group NAME" << endl;
+    cout << "Usage: " << argv[0] << " [-d DET] NAME" << endl;
+    cout << "  Displays the range names group for channel group NAME" << endl;
     cout << "  or the range for channel range NAME." << endl;
+    cout << "  DET = protodune (default) or apa7 or any value for which\n"
+         << "        DET_tools_dune.fcl provides definitions for\n"
+         << "        channelRanges and channelGroups." << endl;
     cout << "  Channel group names include:" << endl;
     cout << "    apas - APAs." << endl;
     cout << "    tpss - TPC sets." << endl;
     cout << "    apaus, apavs, apazs, apacs - APA planes with install naming." << endl;
     cout << "    tppus, tppvs, tppzs, tppcs - APA planes with offline naming." << endl;
     cout << "  Channel range names include:" << endl;
-    cout << "    apa1 - apa6: APA with install numbering." << endl;
+    cout << "    apa1 - apa6: APA with APA numbering." << endl;
     cout << "    tps0 - tps5: APA (TPC set) with offline numbering." << endl;
     cout << "    apa0u, apa0v, apa0z, apa0c, ..., apa5c: APA plane with install numbering." << endl;
     cout << "    tpp0u, tpp0v, tpp0z, tpp0c, ..., tpp5c: APA plane with offline numbering." << endl;
@@ -59,25 +71,40 @@ int main(int argc, char** argv) {
   assert( tm.toolNames().size() >= 1 );
 
   IndexRangeGroupTool* pcgt = tm.getShared<IndexRangeGroupTool>(tnameGroup);
-  if ( pcgt == nullptr ) {
-    cout << "WARNING: Channel group tool not found: " << tnameGroup << endl;
-  } else {
-    IndexRangeGroup cg = pcgt->get(crname);
+  IndexRangeTool* pcrt = tm.getShared<IndexRangeTool>(tnameRange);
+
+  int warnGroup = 0;
+  int warnRange = 0;
+  // If user provides name, exit after displaying result.
+  bool exitLoop = crname.size();
+  while ( true ) {
+    if ( crname.size() == 0 ) {
+      cout << "Group or range name> ";
+      cout.flush();
+      std::getline(cin, crname);
+    }
+    if ( crname.size() == 0  || crname == "q" ) return 0;
+    IndexRangeGroup cg;
+    if ( pcgt == nullptr ) {
+      if ( ! warnGroup++ ) cout << "WARNING: Channel group tool not found: " << tnameGroup << endl;
+    } else {
+      cg = pcgt->get(crname);
+    }
     if ( cg.isValid() ) {
       cout << cg << endl;
-      return 0;
+    } else if ( pcrt == nullptr ) {
+      if ( ! warnRange ) cout << "WARNING: Channel range tool not found: " << tnameRange << endl;
+    } else {
+      IndexRange cr = pcrt->get(crname);
+      if ( cr.isValid() ) {
+        cout << cr << endl;
+      } else {
+        cout << "ERROR: " << crname << " is not a valid channel group or range." << endl;
+      }
     }
+    if ( warnGroup && warnRange ) return 1;
+    if ( exitLoop ) return 0;
+    crname = "";
   }
-  IndexRangeTool* pcrt = tm.getShared<IndexRangeTool>(tnameRange);
-  if ( pcrt == nullptr ) {
-    cout << "WARNING: Channel range tool not found: " << tnameRange << endl;
-  } else {
-    IndexRange cr = pcrt->get(crname);
-    if ( cr.isValid() ) {
-      cout << cr << endl;
-      return 0;
-    }
-  }
-  cout << crname << " is not a valid channel group or range." << endl;
   return 0;
 }
