e8e2027513444351326e0e5553f65336be07ed4a
Jaime Dawson <jvdawson@fnal.gov>
2020-04-02 00:55:26 -0500

    use geoTPC::DetectDriftDirection() to determine drift direction, modify coordinates accordingly.. work in progress. compiles and runs. geometry needs verification

--- dune/Protodune/dualphase/CRTGen_module.cc
+++ dune/Protodune/dualphase/CRTGen_module.cc
@@ -37,10 +37,10 @@ public:
   void beginRun(art::Run & run) 		  override;
 
 private:
-
-  std::vector<double> CRT_TOP_center={-581,305.7,135};//drift in Y geometry in cm
-  std::vector<double> CRT_BOT_center={+581,-202.7,135}; //drift in Y geometry in cm
-  double CRTHeightY=8*14.0; //size in Y
+  short int driftcoordinate=0;
+  std::vector<double> CRT_TOP_centerY={-581,305.7,135};//drift in Y geometry in cm
+  std::vector<double> CRT_BOT_centerY={+581,-202.7,135}; //drift in Y geometry in cm
+  double CRTHeight=8*14.0; //size in Y
   double CRTLengthZ=144; //size in Z
 //  std::ifstream* fInputFile;
 //  std::string    fInputFileName; ///< Name of text file containing events to simulate
@@ -79,7 +79,22 @@ void evgen::CRTGen::beginRun(art::Run& run)
 {
     art::ServiceHandle<geo::Geometry const> geo;
     run.put(std::make_unique<sumdata::RunData>(geo->DetectorName()));
-  }
+    driftcoordinate = (geo->TPC(0)).DetectDriftDirection();
+
+    if( driftcoordinate==1 || driftcoordinate==2 )
+      {
+	std::cout<<" drift coordinate: "<<driftcoordinate<<std::endl;
+      }else{ throw cet::exception("CRTGen") << "unknown drift coordinate "                                                                            << driftcoordinate << " \n"; }  
+
+    /*    +1: positive x
+      +2: positive y
+      +3: positive z
+      -1: negative x
+      -2: negative y
+      -3: negative z
+      0: other (or algorithm failed)
+    */
+ }
 
 //------------------------------------------------------------------------------
 void evgen::CRTGen::produce(art::Event & e)
@@ -95,21 +110,33 @@ void evgen::CRTGen::produce(art::Event & e)
 
   //We simulate crossing muons from CRT TOP to CRT BOT
 
-  std::vector<double> CRT_TOP_center={-581,305.7,135};//drift in Y geometry in cm
-  std::vector<double> CRT_BOT_center={+581,-202.7,135}; //drift in Y geometry in cm
-  double CRTHeightY=8*14.0; //size in Y
+  std::vector<double> CRT_TOP_center=CRT_TOP_centerY; //{-581,305.7,135};//drift in Y geometry in cm
+  std::vector<double> CRT_BOT_center=CRT_BOT_centerY;//{+581,-202.7,135}; //drift in Y geometry in cm
+  double CRTHeightY=CRTHeight; //8*14.0; //size in Y ..TODO..
+  double CRTHeightX=0.0;
   double CRTLengthZ=144; //size in Z
+  //drift in x-coordinate//
+  if(driftcoordinate==1) // Y->X, X->-Y
+    {
+      CRT_TOP_center[0] = CRT_TOP_centerY[1];
+      CRT_TOP_center[1] = CRT_TOP_centerY[0];
+      CRT_BOT_center[0] = CRT_BOT_centerY[1];
+      CRT_BOT_center[1] = CRT_BOT_centerY[0];
+      CRTHeightX=CRTHeight;
+      CRTHeightY=0;
+    }
+
   // declare the variables for reading in the event record
 //  int            status         = 0;
   int 	 	 pdg            = 13;
 //  int 	 	 firstMother    = 0;
-  double 	 energy      	= CLHEP::RandFlat::shoot(2,3); //uniform distribution among 2-3GeV
+  double 	 energy      	= CLHEP::RandFlat::shoot(10,10); //uniform distribution among 2-3GeV
   double 	 mass        	= 0.1056583745;//GeV
-  double 	 xPosition   	= CRT_TOP_center[0];
+  double 	 xPosition   	= CLHEP::RandFlat::shoot(CRT_TOP_center[0]-0.5*CRTHeightX,CRT_TOP_center[0]+0.5*CRTHeightX);
   double 	 yPosition   	= CLHEP::RandFlat::shoot(CRT_TOP_center[1]-0.5*CRTHeightY,CRT_TOP_center[1]+0.5*CRTHeightY);
   double 	 zPosition   	= CLHEP::RandFlat::shoot(CRT_TOP_center[2]-0.5*CRTLengthZ,CRT_TOP_center[2]+0.5*CRTLengthZ);
   double 	 time        	= 0.;
-  double 	 xPositionEnd  	= CRT_BOT_center[0];
+  double 	 xPositionEnd  	= CLHEP::RandFlat::shoot(CRT_BOT_center[0]-0.5*CRTHeightX,CRT_BOT_center[0]+0.5*CRTHeightX);
   double 	 yPositionEnd  	= CLHEP::RandFlat::shoot(CRT_BOT_center[1]-0.5*CRTHeightY,CRT_BOT_center[1]+0.5*CRTHeightY);
   double 	 zPositionEnd  	= CLHEP::RandFlat::shoot(CRT_BOT_center[2]-0.5*CRTLengthZ,CRT_BOT_center[2]+0.5*CRTLengthZ);
 
@@ -127,7 +154,7 @@ void evgen::CRTGen::produce(art::Event & e)
   double 	 xMomentum      = dx*totmom;
   double 	 yMomentum   	= dy*totmom;
   double 	 zMomentum   	= dz*totmom;
-  std::cout << "Shooting muon on " << xPosition << " " << yPosition << " " << zPosition << " With momentum: " << xMomentum << " " << yMomentum << " " << zMomentum << " E=" << energy << " m=" << mass << std::endl;
+  std::cout << "Shooting muon on " << xPosition << " " << yPosition << " " << zPosition <<  "to "<< xPositionEnd << " " << yPositionEnd << " " << zPositionEnd <<" With momentum: " << xMomentum << " " << yMomentum << " " << zMomentum << " E=" << energy << " m=" << mass << std::endl;
 
   TLorentzVector pos(xPosition, yPosition, zPosition, time);
   TLorentzVector mom(xMomentum, yMomentum, zMomentum, energy);
