3e9dfd27ce008bbc3f479a747b2ac782546c382e
David Adams <dladams@bnl.gov>
2020-08-31 13:18:44 -0500

    Add IcebergOnlineTool

--- dune/Protodune/singlephase/Tool/IcebergOnlineChannel_tool.cc
+++ dune/Protodune/singlephase/Tool/IcebergOnlineChannel_tool.cc
@@ -1,24 +1,24 @@
-// PdspOnlineChannel.cxx
+// IcebergOnlineChannel.cxx
 
-#include "PdspOnlineChannel.h"
-#include "dune-raw-data/Services/ChannelMap/PdspChannelMapService.h"
+#include "IcebergOnlineChannel.h"
+#include "dune-raw-data/Services/ChannelMap/IcebergChannelMapService.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include <iostream>
 
 using std::string;
 using std::cout;
 using std::endl;
-using Index = PdspOnlineChannel::Index;
+using Index = IcebergOnlineChannel::Index;
 
 //**********************************************************************
 
-PdspOnlineChannel::PdspOnlineChannel(const fhicl::ParameterSet& pset) 
+IcebergOnlineChannel::IcebergOnlineChannel(const fhicl::ParameterSet& pset) 
 : m_LogLevel(pset.get<Index>("LogLevel")),
   m_Ordering(pset.get<Name>("Ordering")),
   m_orderByWib(m_Ordering == "WIB"),
   m_orderByConnector(m_Ordering == "connector"),
   m_orderByFemb(m_Ordering == "FEMB") {
-  const string myname = "PdspOnlineChannel::ctor: ";
+  const string myname = "IcebergOnlineChannel::ctor: ";
   cout << myname << "  LogLevel: " << m_LogLevel << endl;
   cout << myname << "  Ordering: " << m_Ordering << endl;
   if ( !m_orderByWib && !m_orderByConnector && !m_orderByFemb ) {
@@ -28,16 +28,16 @@ PdspOnlineChannel::PdspOnlineChannel(const fhicl::ParameterSet& pset)
   
 //**********************************************************************
 
-Index PdspOnlineChannel::get(Index chanOff) const {
-  const string myname = "PdspOnlineChannel::get: ";
-  if ( chanOff >= 15360 ) {
+Index IcebergOnlineChannel::get(Index chanOff) const {
+  const string myname = "IcebergOnlineChannel::get: ";
+  if ( chanOff >= 1280 ) {
     if ( m_LogLevel > 1 ) cout << myname << "Invalid offline channel: " << chanOff << endl;
     return badIndex();
   }
-  art::ServiceHandle<dune::PdspChannelMapService> pms;
+  art::ServiceHandle<dune::IcebergChannelMapService> pms;
   // Fetch APA index.
   Index iapa = pms->APAFromOfflineChannel(chanOff);
-  if ( iapa > 5 ) {
+  if ( iapa > 1 ) {
     if ( m_LogLevel > 1 ) cout << myname << "Invalid APA index: " << iapa << endl;
     return badIndex();
   }
@@ -65,22 +65,37 @@ Index PdspOnlineChannel::get(Index chanOff) const {
   Index kchf = ichf;
   // Evauate online index.
   Index chanOn = 0;
+  Index kfmb = 99;
   if ( m_orderByWib ) {
-    chanOn = 2560*kapa + 512*kwib + 128*kcon + kchf;
+    cout << myname << "WIB ordering is not yest supported." << endl;
+    return badIndex();
   } else if ( m_orderByConnector ) {
-    chanOn = 2560*kapa + 640*kcon + 128*kwib + kchf;
+    cout << myname << "Connector ordering is not yest supported." << endl;
   } else if ( m_orderByFemb ) {
     // FEMB index mapping.
-    static Index ifmb[20] = {10,  9,  8,  7,  6,
-                              5,  4,  3,  2,  1,
-                             20, 19, 18, 17, 16,
-                             15, 14, 13, 12, 11};
-    Index jfmb = 5*kcon + kwib;
-    Index kfmb = ifmb[jfmb] - 1;
-    // Beam left, rotate FEMBs by 10.
-    bool beamLeft = kapa/2 != (kapa+1)/2;
-    if ( beamLeft ) kfmb = (kfmb + 10) % 20;
-    chanOn = 2560*kapa + 128*kfmb + kchf;
+    Index ifmb = 99;
+    if        ( chanOff <  200 ) {
+      ifmb = ( 199-chanOff)/40 + 0;
+    } else if ( chanOff <  400 ) {
+      ifmb = ( 399-chanOff)/40 + 5;
+    } else if ( chanOff <  800 ) {
+      ifmb = (chanOff- 400)/40 + 0;
+    } else if ( chanOff < 1040 ) {
+      ifmb = (chanOff- 800)/48 + 0;
+    } else if ( chanOff < 1280 ) {
+      ifmb = (1279-chanOff)/48 + 5;
+    } else {
+      cout << "Invalid offline channel: : " << chanOff << endl;
+      return badIndex();
+    }
+    kfmb = ifmb + 1;
+    chanOn = 1280*kapa + 128*(ifmb) + kchf;
+  }
+  if ( m_LogLevel >= 4 ) {
+    cout << myname << "ChanOff --> (kapa kwib kcon kchf) --> chanON: "
+         << chanOff << " --> (" << kapa << " " << kwib << " " << kcon << " " << kchf
+         << ") -->  " << chanOn
+         << " --> " << 100*(kapa+1) + kfmb << "-" << kchf << endl;
   }
   return chanOn;
 }
