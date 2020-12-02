4de46d25146b5562e12ef0c9f246e48adba4c68f
David Adams <dladams@bnl.gov>
2020-12-02 11:37:43 -0600

    Add decomposition decon opts to AdcDeconvoluteFFT.

--- dune/DataPrep/Tool/AdcDeconvoluteFFT_tool.cc
+++ dune/DataPrep/Tool/AdcDeconvoluteFFT_tool.cc
@@ -4,6 +4,8 @@
 #include "dune/DuneCommon/DuneFFT.h"
 #include "dune/ArtSupport/DuneToolManager.h"
 #include "TMath.h"
+#include "TDecompLU.h"
+#include "TDecompSVD.h"
 #include <iostream>
 #include <iomanip>
 
@@ -16,8 +18,74 @@ using std::setprecision;
 
 using Index = unsigned int;
 using FloatVector = AdcSignalVector;
+using DoubleVector = std::vector<double>;
 using Name = std::string;
 using DFT = DuneFFT::DFT;
+
+//**********************************************************************
+// Local functions.
+//**********************************************************************
+
+namespace {
+
+// Fill a matrix with a vector.
+// The matrix is near diagonal with the near-diagonal columns populated with
+// the entries in the vector vrcol.
+// The diagonal element is vres[ishift].
+//
+// vec - Input smearing vector.
+// ishift - vector offset in matrix
+// mres - Output matrix.
+// smsgpre - If not blank, a message prefixed with this string is logged for each row.
+template<class RootMatrix>
+int responseVectorToMatrix(const DoubleVector& vrcol, Index ishiftin, RootMatrix& mres,
+                           int logLevel, string smsgpre ="") {
+  string myname = smsgpre + "reponseVectorToMatrix: ";
+  // Flip vector so we can fill rows instead of columns.
+  Index nvec = vrcol.size();
+  Index nrow = mres.GetNrows();
+  Index ncol = mres.GetNcols();
+  if ( nvec == 0 ) {
+    if ( logLevel > 0 ) cout << myname << "WARNING: Response vector is empty." << endl;
+    return 1;
+  }
+  if ( ishiftin >= nvec ) {
+    if ( logLevel > 0 ) cout << myname << "WARNING: Shift " << ishiftin
+                             << " is outside vector range (0, " << nvec << "]" << endl;
+    return 2;
+  }
+  if ( logLevel >= 4 ) {
+    cout << myname << "Matrix is " << nrow << " x " << ncol << endl;
+    cout << myname << "Vector has length " << nvec << " and shift " << ishiftin << endl;
+  }
+  Index ishift = nvec - 1 - ishiftin;
+  DoubleVector vrrow(nvec, 0.0);
+  for ( Index ircol=0, irrow=nvec-1; ircol<nvec; ++ircol, --irrow ) {
+    vrrow[irrow] = vrcol[ircol];
+  }
+  // Populate matrix with vector.
+  if ( nvec == 0 ) return 0;
+  for ( Index irow=0; irow<ncol; ++irow ) {
+    Index icol = irow > ishift ? irow - ishift : 0;
+    Index ivec = icol + ishift - irow;
+    Index nvecInsert = icol + nvec < nrow ? nvec - ivec : nrow - icol;
+    if ( logLevel >= 4 ) {
+      cout << smsgpre << "Column " << icol << ": inserting " << nvecInsert
+           << " entries at row " << irow << endl;
+    }
+    mres.InsertRow(irow, icol, &vrrow[ivec], nvecInsert);
+  }
+  return 0;
+}
+
+template<class RootMatrix>
+int responseVectorToMatrix(const FloatVector& vresf, Index ishift, RootMatrix& mres,
+                           int logLevel, string smsgpre ="") {
+  DoubleVector vresd(vresf.begin(), vresf.end());
+  return responseVectorToMatrix(vresd, ishift, mres, logLevel, smsgpre);
+}
+
+}  // End unnamed namepsace.
 
 //**********************************************************************
 // Class methods.
@@ -28,12 +96,15 @@ AdcDeconvoluteFFT::AdcDeconvoluteFFT(fhicl::ParameterSet const& ps)
   m_Action(ps.get<Index>("Action")),
   m_ResponseVectors(ps.get<ResponseVectorVector>("ResponseVectors")),
   m_ResponseCenters(ps.get<IndexVector>("ResponseCenters")),
+  m_SmoothVectors(ps.get<ResponseVectorVector>("SmoothVectors")),
+  m_SmoothScales(ps.get<FloatVector>("SmoothScales")),
   m_GausFilterSigmas(ps.get<FloatVector>("GausFilterSigmas")),
   m_LowFilterWidths(ps.get<FloatVector>("LowFilterWidths")),
   m_LowFilterPowers(ps.get<FloatVector>("LowFilterPowers")),
   m_IndexMapTool(ps.get<Name>("IndexMapTool")),
   m_useResponse(false), m_useFilter(false),
-  m_doFFTConvolute(false), m_doDirectConvolute(false), m_doDeconvolute(false) {
+  m_doFFTConvolute(false), m_doDirectConvolute(false),
+  m_doDeconvolute(false), m_directDeconvolute(0) {
   const Name myname = "AdcDeconvoluteFFT:ctor: ";
   // Set the action flags.
   Name amsg;
@@ -60,8 +131,19 @@ AdcDeconvoluteFFT::AdcDeconvoluteFFT(fhicl::ParameterSet const& ps)
     m_doDirectConvolute = true;
     m_useFilter = true;
     amsg = "Direct filter convolution.";
+  } else if ( m_Action == 6 ) {
+    m_directDeconvolute = 1;
+    amsg = "Direct matrix deconvolution.";
+  } else if ( m_Action == 7 ) {
+    m_directDeconvolute = 2;
+    amsg = "Filtered matrix deconvolution.";
+  } else if ( m_Action == 8 ) {
+    m_directDeconvolute = 3;
+    amsg = "Chi-square matrix deconvolution.";
+  } else if ( m_Action == 9 ) {
+    m_directDeconvolute = 4;
+    amsg = "Direct deconvolution.";
   } else {
-    abort();
     cout << myname << "ERROR: Invalid action flag: " << m_Action << endl;
   }
   // Fetch index mapper.
@@ -97,7 +179,7 @@ AdcDeconvoluteFFT::AdcDeconvoluteFFT(fhicl::ParameterSet const& ps)
     }
     cout << endl;
     cout << myname << setw(indent-2) << "]" << endl;
-    cout << myname << "    ResponseShifts: [";
+    cout << myname << "   ResponseShifts: [";
     first = true;
     for ( Index val : m_ResponseCenters ) {
       if ( first ) first = false;
@@ -105,7 +187,32 @@ AdcDeconvoluteFFT::AdcDeconvoluteFFT(fhicl::ParameterSet const& ps)
       cout << val;
     }
     cout << "]" << endl;
-    cout << myname << "  GausFilterSigmas: [";
+    cout << myname << "    SmoothVectors: [";
+    indent = 22;
+    first = true;
+    for ( const ResponseVector& vec : m_SmoothVectors ) {
+      if ( first ) first = false;
+      else cout << ", ";
+      for ( Index isam=0; isam<vec.size(); ++isam ) {
+        float val = vec[isam];
+        if ( isam != 0 ) cout << ", ";
+        if ( isam == 0 ) cout << "\n" << myname << setw(indent) << "[";
+        else if ( 10*(isam/10) == isam ) cout << "\n" << myname << setw(indent) << " ";
+        cout << setw(11) << fixed << setprecision(7) << val;
+      }
+      cout << "]";
+    }
+    cout << endl;
+    cout << myname << setw(indent-2) << "]" << endl;
+    cout << myname << "     SmoothScales: [";
+    first = true;
+    for ( float val : m_SmoothScales ) {
+      if ( first ) first = false;
+      else cout << ", ";
+      cout << setw(11) << fixed << setprecision(7) << val;
+    }
+    cout << "]" << endl;
+    cout << myname << " GausFilterSigmas: [";
     first = true;
     for ( float sgm : m_GausFilterSigmas ) {
       if ( first ) first = false;
@@ -129,7 +236,7 @@ AdcDeconvoluteFFT::AdcDeconvoluteFFT(fhicl::ParameterSet const& ps)
       cout << val;
     }
     cout << "]" << endl;
-    cout << myname << "    ChannelMapTool: " << m_IndexMapTool << endl;;
+    cout << myname << "   ChannelMapTool: " << m_IndexMapTool << endl;;
     std::cout.copyfmt(cout_state);
   }
 }
@@ -159,6 +266,9 @@ DataMap AdcDeconvoluteFFT::update(AdcChannelData& acd) const {
          << ivec << " (channel " << icha << ")." << endl;
     return ret;
   }
+  const ResponseVector empty;
+  const ResponseVector& smoothVector = ivec < m_SmoothVectors.size() ? m_SmoothVectors[ivec] : empty;
+  float smoothScale = ivec < m_SmoothScales.size() ? m_SmoothScales[ivec] : 1.0;
   float gausFilterSigma = m_GausFilterSigmas[ivec];
   float lfwidth = -1.0;
   float lfpower = 0;
@@ -180,7 +290,7 @@ DataMap AdcDeconvoluteFFT::update(AdcChannelData& acd) const {
   Index rstat = 0;
   Index fftLogLevel = m_LogLevel > 2 ? m_LogLevel - 2 : 0.0;
 
-  if ( !m_doDeconvolute && !m_doFFTConvolute && !m_doDirectConvolute ) return ret;
+  if ( !m_doDeconvolute && !m_doFFTConvolute && !m_doDirectConvolute && !m_directDeconvolute ) return ret;
 
   bool useFFT = m_doDeconvolute || m_doFFTConvolute;
 
@@ -367,11 +477,105 @@ DataMap AdcDeconvoluteFFT::update(AdcChannelData& acd) const {
     }
   }
 
-  // Record results.
-  acd.samples = xdasOut;
-  if ( xamsOut.size() ) {
-    acd.dftmags = xamsOut;
-    acd.dftphases = xphsOut;
+  // Deconvolution with matrix solve.
+  if ( m_directDeconvolute ) {
+    if ( m_LogLevel >= 1 ) cout << myname << "Deconvoluting with Root: option "
+                                << m_directDeconvolute << "." << endl;
+    // Construct the response matrix from the response vector.
+    TMatrixDSparse mres(nsam, nsam);
+    if ( responseVectorToMatrix(responseVector, ishift, mres, m_LogLevel, myname) ) return ret.setStatus(6);
+    // Build data vector.
+    TVectorD vdat(nsam);
+    for ( Index isam=0; isam<nsam; ++isam ) {
+      vdat[isam] = xdasInp[isam];
+    }
+    // The following are alternatives to direct solution of vdat = mres q.
+    // The direct solution doesn't work because mres is ill conditioned.
+    // Either mres or mres and vdat are updated.
+    if ( m_directDeconvolute == 1 ) {
+      cout << "Performing direct matrix deconvolution." << endl;
+    } else if ( m_directDeconvolute == 2 ) {
+      cout << "Performing filtered matrix deconvolution." << endl;
+      // Filtered deconvolution.
+      // vdat = (mres mfil_inv) (mfil q)
+      // Doesn't work because mfil is ill conditioned.
+      // Build smearing vector vfil and matrix mfil.
+      DoubleVector vfil(smoothVector.begin(), smoothVector.end());
+      Index nfil = vfil.size();
+      TMatrixDSparse mfil(nsam, nsam);
+      if ( responseVectorToMatrix(vfil, nfil/2, mfil, m_LogLevel, myname) ) return ret.setStatus(7);
+      // Solve R_t = S_t (R Sinv)_t to get RSinv.
+      TMatrixDSparse mfil_t(nsam,nsam);
+      mfil_t.Transpose(mfil);
+      TMatrixD rsinv_t(nsam,nsam);
+      rsinv_t.Transpose(mres);
+      TDecompSVD ssolver(mfil_t);
+      if ( ssolver.Decompose() ) {
+        cout << myname << "Filtering condition is " << ssolver.Condition() << endl;
+        cout << myname << "S_t is " << (ssolver.GetMatrix().IsValid() ? "" : "not ") << "valid."  << endl;
+        cout << myname << "R_t is " << (rsinv_t.IsValid() ? "" : "not ") << "valid."  << endl;
+        ssolver.MultiSolve(rsinv_t);
+        cout << myname << "(R Sinv)_t is " << (rsinv_t.IsValid() ? "" : "not ") << "valid."  << endl;
+        // Replace R with RSinv.
+        mres.Transpose(rsinv_t);
+      } else {
+        cout << myname << "Unable to decompose smearing matrix!" << endl;
+        return ret.setStatus(8);
+      }
+    // y = (R_t R + S_t S_t S) x
+    } else if ( m_directDeconvolute == 3 ) {
+      cout << "Performing chi-square matrix deconvolution." << endl;
+      TMatrixDSparse mres_t(nsam, nsam);
+      mres_t.Transpose(mres);
+      TMatrixDSparse mresres(mres_t);
+      mresres *= mres;
+      vdat *= mres_t;
+      vdat.Print();
+      // Fetch smearing vector vsmr and build matrix msmr.
+      DoubleVector vsmr(smoothVector.begin(), smoothVector.end());
+      for ( double& val : vsmr ) val*= smoothScale;
+      Index nsmr = vsmr.size();
+      TMatrixDSparse msmr(nsam, nsam);
+      if ( responseVectorToMatrix(vsmr, nsmr/2, msmr, m_LogLevel, myname) ) return ret.setStatus(9);
+      TMatrixDSparse msmr_t(nsam, nsam);
+      msmr_t.Transpose(msmr);
+      TMatrixDSparse msmrsmr(msmr_t);
+      msmrsmr *= msmr;
+      // Add smearing to reponse.
+      mres.SetSparseIndexAB(mresres, msmrsmr);
+      mres = mresres + msmrsmr;
+    } else if ( m_directDeconvolute == 4 ) {
+      DoubleVector vsmr = {0.05, 0.20, 0.5, 0.20, 0.05};
+      Index nsmr = vsmr.size();
+      TMatrixDSparse msmr(nsam, nsam);
+      if ( responseVectorToMatrix(vsmr, nsmr/2, msmr, m_LogLevel, myname) ) return ret.setStatus(9);
+      //vdat *= msmr;
+      msmr *= mres;
+      mres.SetSparseIndex(msmr);
+      mres = msmr;
+    } else if ( m_directDeconvolute != 1 ) {
+      return ret.setStatus(10);
+    }
+    // Solve vdat = mres q
+    //TDecompLU solver(mres);
+    TDecompSVD solver(mres);
+    if ( solver.Decompose() ) {
+      cout << myname << "Final matrix condition is " << solver.Condition() << endl;
+      solver.Solve(vdat);
+      for ( Index isam=0; isam<nsam; ++isam ) {
+        acd.samples[isam] = vdat[isam];
+      }
+    } else {
+      cout << myname << "Unable to decompose response matrix!" << endl;
+      return ret.setStatus(11);
+    }
+  } else {
+    // Record results from any other operation.
+    acd.samples = xdasOut;
+    if ( xamsOut.size() ) {
+      acd.dftmags = xamsOut;
+      acd.dftphases = xphsOut;
+    }
   }
 
   return ret;
