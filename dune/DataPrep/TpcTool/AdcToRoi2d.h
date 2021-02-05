03f6ef75cd0fb3baac5b7f9e4a3e6ec68bd266a3
David Adams <dladams@bnl.gov>
2021-02-05 08:30:20 -0600

    Add TpcTool dir with AdctoToi2d tool AdctoToi2d

---
+++ dune/DataPrep/TpcTool/AdcToRoi2d.h
@@ -0,0 +1,52 @@
+// AdcToRoi2d.h
+//
+// David Adams
+// February 2021
+//
+// Tool to convert maps of ADC channel data into 2D ROIs.
+//
+// Configuration:
+//            LogLevel - Logging level: 0=none, 1=init, 2=call, ...
+//              Option - Action to take:
+//                         0 - none except logging
+//                         1 - One ROI for each channel map
+//                         2 - All channel maps merged into a singe ROI.
+// Returned data map:
+//   a2r_nroi - # ROIS created                        
+//   a2r_nsams - # samples for each ROI                
+
+#ifndef AdcToRoi2d_H
+#define AdcToRoi2d_H
+
+#include "art/Utilities/ToolMacros.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "dune/DuneInterface/Tool/TpcDataTool.h"
+#include "dune/DuneInterface/Data/IndexRange.h"
+#include "dune/DuneInterface/Data/RunData.h"
+#include "dune/DuneCommon/TPadManipulator.h"
+#include <iostream>
+
+class AdcToRoi2d : TpcDataTool {
+
+public:
+
+  using Index = unsigned int;
+
+  AdcToRoi2d(fhicl::ParameterSet const& ps);
+
+  ~AdcToRoi2d() override;
+
+  // TpcDataTool methods.
+  DataMap updateTpcData(TpcData& tpd) const override;
+
+private:
+
+  // Configuration data.
+  int m_LogLevel;
+  Index m_Option;
+
+};
+
+DEFINE_ART_CLASS_TOOL(AdcToRoi2d)
+
+#endif
---
+++ dune/DataPrep/TpcTool/AdcToRoi2d_tool.cc
@@ -0,0 +1,163 @@
+// AdcToRoi2d_tool.cc
+
+#include "AdcToRoi2d.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::vector;
+using fhicl::ParameterSet;
+
+using Index = AdcToRoi2d::Index;
+using LongIndex = unsigned long int;
+
+namespace {
+
+string optionString(Index iopt) {
+  if ( iopt == 0 ) return "No copy";
+  if ( iopt == 1 ) return "One ROI/map";
+  if ( iopt == 2 ) return "One ROI for all maps";
+  return "Invalid option";
+}
+
+}  // end unnamed namespace
+
+//**********************************************************************
+// Class methods.
+//**********************************************************************
+
+AdcToRoi2d::AdcToRoi2d(fhicl::ParameterSet const& ps)
+: m_LogLevel(ps.get<int>("LogLevel")),
+  m_Option(ps.get<Index>("Option")) {
+  const string myname = "AdcToRoi2d::ctor: ";
+  if ( m_Option > 2 ) {
+    cout << myname << "WARNING: Invalid option: " << m_Option
+         << " will be treated as 0." << endl;
+  }
+  // Display the configuration.
+  if ( m_LogLevel>= 1 ) {
+    cout << myname << "             LogLevel: " << m_LogLevel << endl;
+    cout << myname << "               Option: " << m_Option
+         << " (" << optionString(m_Option) << ")" << endl;
+  }
+}
+
+//**********************************************************************
+
+AdcToRoi2d::~AdcToRoi2d() { }
+
+//**********************************************************************
+
+DataMap AdcToRoi2d::updateTpcData(TpcData& tpd) const {
+  const string myname = "AdcToRoi2d::updateTpcData: ";
+  DataMap ret;
+  Index imax = -1;
+  LongIndex lmax = -1;
+  // Find the channel and tick ranges for each ADC map.
+  Index nacm = tpd.getAdcData().size();
+  vector<LongIndex> itck1s(nacm, lmax);
+  vector<LongIndex> itck2s(nacm, 0);
+  vector<Index> icha1s(nacm, imax);
+  vector<Index> icha2s(nacm, 0);
+  vector<Index> ndats(nacm, 0);
+  Index nerr = 0;
+  if ( m_LogLevel >= 3 ) cout << myname << "Channel map count: " << nacm << endl;
+  for ( Index iacm=0; iacm<nacm; ++iacm ) {
+    LongIndex ndat = 0;
+    const TpcData::AdcDataPtr pacm = tpd.getAdcData().at(iacm);
+    if ( ! pacm ) {
+      if ( m_LogLevel >=2 ) cout << myname << "Skipping missing ADC channel map." << endl;
+      ++nerr;
+      continue;
+    }
+    if ( pacm->size() == 0 ) {
+      if ( m_LogLevel >=2 ) cout << myname << "Skipping empty ADC channel map." << endl;
+      ++nerr;
+      continue;
+    }
+    if ( m_LogLevel >= 3 ) cout << myname << "  Channel count: " << pacm->size() << endl;
+    for ( const auto& iacd : *pacm ) {
+      const AdcChannelData& acd = iacd.second;
+      Index nsam = acd.samples.size();
+      Index icha1 = acd.channel();
+      Index icha2 = icha1 + 1;
+      Index itck1 = acd.tickOffset();
+      Index itck2 = itck1 + nsam;
+      if ( icha1 < icha1s[iacm] ) icha1s[iacm] = icha1;
+      if ( icha2 > icha2s[iacm] ) icha2s[iacm] = icha2;
+      if ( itck1 < itck1s[iacm] ) itck1s[iacm] = itck1;
+      if ( itck2 > itck2s[iacm] ) itck2s[iacm] = itck2;
+      if ( m_LogLevel >= 3 ) cout << myname << "    Channel " << acd.channel()
+                                  << " sample count: " << nsam << endl;
+      ndat += nsam;
+    }
+    if ( icha1s[iacm] >= icha2s[iacm] ) {
+      cout << myname << "ERROR: Channels out of range. Skipping ADC map " << iacm << "." << endl;
+      ++nerr;
+      continue;
+    }
+    if ( ndat == 0 ) {
+      cout << myname << "ERROR: No samples found. Skipping ADC map " << iacm << "." << endl;
+      ++nerr;
+      continue;
+    }
+    if ( itck1s[iacm] >= itck2s[iacm] ) {
+      cout << myname << "ERROR: Ticks out of range. Skipping ADC map " << iacm << "." << endl;
+      ++nerr;
+      continue;
+    }
+    ndats[iacm] = ndat;
+    LongIndex ncha = icha2s[iacm] - icha1s[iacm];
+    LongIndex ntck = itck2s[iacm] - itck1s[iacm];
+    LongIndex ncht = ncha*ntck;
+    double frac = double(ndat)/double(ncht);
+    if ( m_LogLevel >= 2 ) {
+      cout << myname << "  ADC map is " << ncha << " x " << ntck
+           << " with fill fraction " << frac << endl;
+      cout << myname << "  Channel range: [ " << icha1s[iacm] << ", " << icha2s[iacm] << ")" << endl;
+      cout << myname << "  Tick range: [ " << itck1s[iacm] << ", " << itck2s[iacm] << ")" << endl;
+    }
+  }
+  Index nroi = 0;
+  vector<int> roiNdats;
+  if ( m_Option == 1 ) {
+    for ( Index iacm=0; iacm<nacm; ++iacm ) {
+      Index icha1 = icha1s[iacm];
+      Index icha2 = icha2s[iacm];
+      LongIndex itck1 = itck1s[iacm];
+      LongIndex itck2 = itck2s[iacm];
+      if ( icha1 >= icha2 ) continue;
+      if ( itck1 >= itck2 ) continue;
+      Index ncha = icha2 - icha1;
+      Index ntck = Index(itck2 - itck1);
+      // Create and fill ROI.
+      tpd.get2dRois().emplace_back(ncha, ntck, icha1, itck1);
+      ++nroi;
+      Tpc2dRoi& roi = tpd.get2dRois().back();
+      const TpcData::AdcDataPtr pacm = tpd.getAdcData().at(iacm);
+      Tpc2dRoi::DataArray::IndexArray idxs;
+      Index ndat = 0;
+      for ( const auto& iacd : *pacm ) {
+        const AdcChannelData& acd = iacd.second;
+        idxs[0] = acd.channel() - icha1;
+        idxs[1] = acd.tickOffset() - itck1;
+        for ( Index isam=0; isam<acd.samples.size(); ++isam, ++idxs[1], ++ndat ) {
+          roi.data().setValue(idxs, acd.samples[isam]);
+        }
+      }
+      roiNdats.push_back(ndat);
+      if ( ndat != ndats[iacm] ) {
+        cout << myname << "ERROR: Unexpected fill count: " << ndat << " != " << ndats[iacm] << endl;
+        ++nerr;
+      }
+    }
+  } else if ( m_Option == 2 ) {
+    cout << myname << "ERROR: Merging of SDC data is not yet supported." << endl;
+  }
+  ret.setStatus(nerr);
+  ret.setInt("a2r_nroi", nroi);
+  ret.setIntVector("a2r_nsams", roiNdats);
+  return ret;
+}
+
+//**********************************************************************
