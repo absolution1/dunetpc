81bea326a56cd2e4a39217a243f71360c5198820
David Adams <dladams@bnl.gov>
2021-06-11 09:13:27 -0500

    AdcSampleScxlaer: Fix OutputUnit

--- dune/DataPrep/Tool/AdcSampleScaler.h
+++ dune/DataPrep/Tool/AdcSampleScaler.h
@@ -5,7 +5,7 @@
 // Configuration:
 //   LogLevel: 0=silent, 1=init, 2=each event, >2=more
 //   ScaleFactor: Factor by which samples are scaled.
-//   OutputUnit: Value assigned for sampleUnit.
+//   OutputUnit: Value assigned for sampleUnit. If balnk, old value is retained.
 //   InputUnit: If non-blank, then this is compared with sampleUnit and a warning
 //               broadcast if they differ.
 
