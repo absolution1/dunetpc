f099b6b2fd7393700d84db8bb46ab890621948be
Jacob Calcutt <calcuttj@msu.edu>
2021-10-19 19:16:06 -0500

    Recreated some hdf5 utils in VDColdbox

--- dune/VDColdbox/RawDecoding/VDColdboxHDF5RawInput.h
+++ dune/VDColdbox/RawDecoding/VDColdboxHDF5RawInput.h
@@ -13,9 +13,12 @@
 #include "art/Framework/Principal/SubRunPrincipal.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "canvas/Persistency/Provenance/FileFormatVersion.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
 #include "fhiclcpp/ParameterSet.h"
-#include "dune-raw-data/HDFUtils/HDFFileReader.hh"
+//#include "dune-raw-data/HDFUtils/HDFFileReader.hh"
+#include "dune/VDColdbox/RawDecoding/VDColdboxHDF5Utils.h"
 
+#include "lardataobj/RawData/RDTimeStamp.h"
 #include "dune/DuneObj/DUNEHDF5FileInfo.h"
 
 
@@ -33,18 +36,19 @@ class raw::VDColdboxHDF5RawInputDetail {
     : pretend_module_name(ps.get<std::string>("raw_data_label", "daq")),
       pmaker(sh) {
     rh.reconstitutes<raw::DUNEHDF5FileInfo, art::InEvent>(pretend_module_name); 
+    rh.reconstitutes<raw::RDTimeStamp, art::InEvent>(pretend_module_name);
   };
 
   void readFile(std::string const & filename, art::FileBlock*& fb) {
-    hdf_file_ = dune::HDFFileReader::openFile(filename);
+    hdf_file_ = dune::VDColdboxHDF5Utils::openFile(filename);
     unprocessedEventList_
-        = dune::HDFFileReader::getTopLevelGroupNames(hdf_file_);
-    TLOG_INFO("VDColdboxHDF5")
+        = dune::VDColdboxHDF5Utils::getTopLevelGroupNames(hdf_file_);
+    MF_LOG_INFO("VDColdboxHDF5")
         << "VDColdboxHDF5 opened HDF file with run number " <<
            hdf_file_->runNumber  << " and " <<
            unprocessedEventList_.size() << " events";
     for (const auto & e : unprocessedEventList_)
-      TLOG_INFO("VDColdboxHDF5") << e;
+      MF_LOG_INFO("VDColdboxHDF5") << e;
 
     fb = new art::FileBlock(art::FileFormatVersion(1, "RawEvent2011"),
                             filename); 
@@ -68,13 +72,13 @@ class raw::VDColdboxHDF5RawInputDetail {
     art::Timestamp currentTime = 0;
     timespec hi_res_time;
     int retcode = clock_gettime(CLOCK_REALTIME, &hi_res_time);
-    TLOG_INFO("VDColdboxHDF5") << "hi_res_time tv_sec = " << hi_res_time.tv_sec
+    MF_LOG_INFO("VDColdboxHDF5") << "hi_res_time tv_sec = " << hi_res_time.tv_sec
                                << " tv_nsec = " << hi_res_time.tv_nsec << " (retcode = " << retcode << ")";
     if (retcode == 0) {
       currentTime = ((hi_res_time.tv_sec & 0xffffffff) << 32) | (hi_res_time.tv_nsec & 0xffffffff);
     }
     else {
-      TLOG_ERROR("VDColdboxHDF5")
+      MF_LOG_ERROR("VDColdboxHDF5")
         << "Unable to fetch a high-resolution time with clock_gettime for art::Event Timestamp. "
         << "The art::Event Timestamp will be zero for event ";
     }
@@ -108,8 +112,10 @@ class raw::VDColdboxHDF5RawInputDetail {
         new DUNEHDF5FileInfo(hdf_file_->fileName, hdf_file_->filePtr,
                              0, nextEventGroupName));
 
-    //Replace with dune hdf 5 info
-    put_product_in_principal(std::move(the_info), *outE, pretend_module_name, "");
+    put_product_in_principal(std::move(the_info), *outE, pretend_module_name,
+                             "");
+    //put_product_in_principal(std::move(the_info), *outE, pretend_module_name,
+    //                         "trigger");
 
     return true;
   };
@@ -117,7 +123,7 @@ class raw::VDColdboxHDF5RawInputDetail {
   void closeCurrentFile() {};
 
  private:
-  std::unique_ptr<dune::HDFFileReader::HDFFileInfo> hdf_file_;
+  std::unique_ptr<dune::VDColdboxHDF5Utils::HDFFileInfo> hdf_file_;
   std::list<std::string> unprocessedEventList_;
   std::string pretend_module_name;
   art::SourceHelper const& pmaker;
